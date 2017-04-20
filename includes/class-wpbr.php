e954b5a1d50fbab51a20a28da73f5c3d9a150242
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-04-20 14:40:57 -0400

    Require the WPBR_Response class

--- includes/class-wpbr.php
+++ includes/class-wpbr.php
@@ -109,6 +109,11 @@ class WPBR {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-i18n.php';
 
 		/**
+		 * Abstract class responsible for normalizing the response from the supported reviews APIs.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-response.php';
+
+		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wpbr-admin.php';
