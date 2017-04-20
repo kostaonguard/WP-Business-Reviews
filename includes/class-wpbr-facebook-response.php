93718f6c33482cb1267c83e1b41fe8e8dc4a324c
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-04-20 18:59:58 -0400

    Extend abstract response class for Facebook

---
+++ includes/class-wpbr-facebook-response.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Normalize the response from the Facebook Graph API
+ *
+ * @link       https://wordimpress.com
+ * @since      1.0.0
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ */
+
+/**
+ * Normalize the response from the Facebook Graph API.
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ * @author     WordImpress, LLC <info@wordimpress.com>
+ */
+class WPBR_Facebook_Response extends WPBR_Response {
+
+	/**
+	 * Set the business name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_name( $data ) {
+
+		$this->business_name = $data['name'];
+
+	}
+
+	/**
+	 * Set the URL of the business page on the reviews platform.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_platform_url( $data ) {
+		// TODO: Implement set_business_platform_url() method.
+	}
+
+	/**
+	 * Set the URL of the business image or avatar.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_image_url( $data ) {
+		// TODO: Implement set_business_image_url() method.
+	}
+
+	/**
+	 * Set the business rating.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_rating( $data ) {
+
+		$this->business_rating = $data['overall_star_rating'];
+
+	}
+
+	/**
+	 * Set the total number of business reviews.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_review_count( $data ) {
+
+		$this->business_review_count = $data['rating_count'];
+
+	}
+
+	/**
+	 * Set the formatted business phone number.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_phone( $data ) {
+
+		$this->business_phone = $data['phone'];
+
+	}
+
+	/**
+	 * Set the formatted business address.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_business_address( $data ) {
+
+		$this->business_address = $data['single_line_address'];
+
+	}
+
+	/**
+	 * Set the collection of individual reviews.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data Array of reviews data, varies by platform.
+	 */
+	public function set_reviews( $data ) {
+		// TODO: Implement set_reviews() method.
+	}
+
+}
