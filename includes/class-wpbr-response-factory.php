6c143be4a7d2150e3290fb5999b4540e36155b85
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-04-20 14:42:38 -0400

    Add factory class to determine platform response

---
+++ includes/class-wpbr-response-factory.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Determine the appropriate type of API response to request based on platform
+ *
+ * @link       https://wordimpress.com
+ * @since      1.0.0
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ */
+
+/**
+ * Determine the appropriate type of API response to request based on platform.
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ * @author     WordImpress, LLC <info@wordimpress.com>
+ */
+class WPBR_Response_Factory {
+
+	/**
+	 * Initialize the concrete class that is appropriate for the platform.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $platform Reviews platform associated with the business.
+	 * @param string $business_id ID of the business.
+	 *
+	 * @return WPBR_Response Normalized data from the reviews platform API.
+	 */
+	public static function create( $platform, $business_id ) {
+
+		switch ( $platform ) {
+
+			case 'google_places' :
+
+				return new WP_Business_Reviews_Google_Places_Response( $platform, $business_id );
+
+			case 'facebook' :
+
+				return new WP_Business_Reviews_Facebook_Response( $platform, $business_id );
+
+			case 'yelp' :
+
+				return new WP_Business_Reviews_Yelp_Response( $platform, $business_id );
+
+			case 'yp' :
+
+				return new WP_Business_Reviews_YP_Response( $platform, $business_id );
+
+		}
+
+	}
+
+}
