895a37c118de44ac49dc7b8e334eb64948be1b49
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-04-21 03:47:19 -0400

    Add WPBR_Review abstract class

---
+++ includes/class-wpbr-review.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Defines the WPBR_Review abstract class
+ *
+ * @link       https://wordimpress.com
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ * @since      1.0.0
+ */
+
+/**
+ * Implements the WPBR_Review object which contains normalized review data
+ * that has been parsed from a remote API response.
+ *
+ * @since 1.0.0
+ */
+abstract class WPBR_Review {
+	// TODO: Define WPBR_Review class.
+}
