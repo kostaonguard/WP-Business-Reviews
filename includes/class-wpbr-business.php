e0663be7e6a406660a0f5728c9fd5ecbb6b647f6
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-04-21 03:47:06 -0400

    Add WPBR_Business abstract class

---
+++ includes/class-wpbr-business.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Defines the WPBR_Business abstract class
+ *
+ * @link       https://wordimpress.com
+ *
+ * @package    WPBR
+ * @subpackage WPBR/includes
+ * @since      1.0.0
+ */
+
+/**
+ * Implements the WPBR_Business object which contains normalized business data
+ * that has been parsed from a remote API response.
+ *
+ * @since 1.0.0
+ */
+abstract class WPBR_Business {
+	// TODO: Define WPBR_Business class.
+}
