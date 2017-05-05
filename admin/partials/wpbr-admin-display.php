4b4a36f4f07a5e201db6d04f9574f7917e3fe938
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-05-05 18:42:55 -0400

    Add forms for dynamic testing

--- admin/partials/wpbr-admin-display.php
+++ admin/partials/wpbr-admin-display.php
@@ -15,36 +15,99 @@
 
 <div class="wrap">
 
-	<h1>WP Business Reviews Testing</h1>
+	<?php
+	$google_places_business_id = '';
+	$facebook_business_id      = '';
+	$yelp_business_id          = '';
+	$yp_business_id            = '';
 
-	<p>Check out these normalized API responses from four different platforms!</p>
+	if ( ! empty( $_POST ) ) {
+
+		$google_places_business_id = isset( $_POST[ 'google_places_business_id' ] ) ? $_POST['google_places_business_id'] : '';
+		$facebook_business_id = isset( $_POST[ 'facebook_business_id' ] ) ? $_POST['facebook_business_id'] : '';
+		$yelp_business_id = isset( $_POST[ 'yelp_business_id' ] ) ? $_POST['yelp_business_id'] : '';
+		$yp_business_id = isset( $_POST[ 'yp_business_id' ] ) ? $_POST['yp_business_id'] : '';
+
+	}
+	?>
+
+	<h1>WP Business Reviews Testing</h1>
 
 	<h2>Google Places</h2>
 
+	<a href="https://developers.google.com/places/place-id">Find a Google Places ID</a><br><br>
+
+	<form method="post" action="/wp-admin/options-general.php?page=wpbr">
+		<label for="google_places_business_id">Google Places ID:</label>
+		<input type="text" id="google_places_business_id" name="google_places_business_id" value="<?php esc_attr_e( $google_places_business_id ); ?>">
+		<button type="submit">Submit</button>
+	</form>
+
 	<?php
-//	$google_business = new WPBR_Google_Places_Business( GOOGLE_PLACES_ID, 'google_places' );
-//	echo '<pre>'; print_r( $google_business ); echo '</pre>';
+	if ( ! empty( $google_places_business_id ) ) {
+
+		$google_places_business = new WPBR_Google_Places_Business( $_POST[ 'google_places_business_id' ], 'google_places' );
+		echo '<pre>'; print_r( $google_places_business ); echo '</pre>';
+
+	}
 	?>
 
 	<h2>Facebook</h2>
 
+	<a href="https://developers.facebook.com/tools/explorer/">Find a Facebook Business ID</a><br>
+	<p>Note: You may need to update the Facebook Page Access Token since they expire quickly.</p>
+
+	<form method="post" action="/wp-admin/options-general.php?page=wpbr">
+		<label for="facebook_business_id">Facebook Business ID:</label>
+		<input type="text" id="facebook_business_id" name="facebook_business_id" value="<?php esc_attr_e( $facebook_business_id ); ?>">
+		<button type="submit">Submit</button>
+	</form>
+
 	<?php
-//	$facebook_business = new WPBR_Facebook_Business( FACEBOOK_PAGE_ID, 'facebook' );
-//	echo '<pre>'; print_r( $facebook_business ); echo '</pre>';
+	if ( ! empty( $facebook_business_id ) ) {
+
+		$facebook_business = new WPBR_Facebook_Business( $_POST[ 'facebook_business_id' ], 'facebook' );
+		echo '<pre>'; print_r( $facebook_business ); echo '</pre>';
+
+	}
 	?>
 
 	<h2>Yelp</h2>
-	<?php
-	$yelp_business = new WPBR_Yelp_Business( YELP_BUSINESS_ID, 'yelp' );
-	echo '<pre>'; print_r( $yelp_business ); echo '</pre>';
-	?>
 
+	<p>The Yelp Business ID is the last part of the URL following /biz/.</p>
+
+	<form method="post" action="/wp-admin/options-general.php?page=wpbr">
+		<label for="yelp_business_id">Yelp Business ID:</label>
+		<input type="text" id="yelp_business_id" name="yelp_business_id" value="<?php esc_attr_e( $yelp_business_id ); ?>">
+		<button type="submit">Submit</button>
+	</form>
+
+	<?php
+	if ( ! empty( $yelp_business_id ) ) {
+
+		$yelp_business = new WPBR_Yelp_Business( $_POST[ 'yelp_business_id' ], 'yelp' );
+		echo '<pre>'; print_r( $yelp_business ); echo '</pre>';
+
+	}
+	?>
 
 	<h2>YP</h2>
 
+	<p>The YP Listing ID is the last part of the URL following ?lid=.</p>
+
+	<form method="post" action="/wp-admin/options-general.php?page=wpbr">
+		<label for="yp_business_id">YP Listing ID:</label>
+		<input type="text" id="yp_business_id" name="yp_business_id" value="<?php esc_attr_e( $yp_business_id ); ?>">
+		<button type="submit">Submit</button>
+	</form>
+
 	<?php
-//	$yp_business = new WPBR_YP_Business( YP_LISTING_ID, 'yp' );
-//	echo '<pre>'; print_r( $yp_business ); echo '</pre>';
+	if ( ! empty( $yp_business_id ) ) {
+
+		$yp_business = new WPBR_YP_Business( $_POST[ 'yp_business_id' ], 'yp' );
+		echo '<pre>'; print_r( $yp_business ); echo '</pre>';
+
+	}
 	?>
 
 </div>
