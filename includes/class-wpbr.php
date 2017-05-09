969f53037603533a25c927608ae7e8b0cb4d7021
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-05-09 02:06:52 -0400

    Require WPBR_Request classes

--- includes/class-wpbr.php
+++ includes/class-wpbr.php
@@ -115,13 +115,13 @@ class WPBR {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-yp-business.php';
 
 		/**
-		 * Classes responsible for API calls to various reviews platforms.
+		 * Classes responsible for API requests to various reviews platforms.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-api-call.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-google-places-api-call.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-facebook-business-api-call.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-yelp-business-api-call.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-yp-business-api-call.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-request.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-google-places-request.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-facebook-request.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-yelp-request.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpbr-yp-request.php';
 
 		/**
 		 * Class responsible for defining all actions that occur in the admin area.
