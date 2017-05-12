54196159b741f199415c8188b08895b931fc26d1
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-05-12 03:17:08 -0400

    Sanitize API data in setter methods

--- includes/class-wpbr-google-places-business.php
+++ includes/class-wpbr-google-places-business.php
@@ -123,7 +123,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_business_name_from_api( $data ) {
 
-		$this->business_name = isset( $data['name'] ) ? $data['name'] : '';
+		$this->business_name = isset( $data['name'] ) ? sanitize_text_field( $data['name'] ) : '';
 
 	}
 
@@ -136,7 +136,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_platform_url_from_api( $data ) {
 
-		$this->platform_url = isset( $data['url'] ) ? $data['url'] : '';
+		$this->platform_url = isset( $data['url'] ) && filter_var( $data['url'], FILTER_VALIDATE_URL ) ? $data['url'] : '';
 
 	}
 
@@ -149,7 +149,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_rating_from_api( $data ) {
 
-		$this->rating = isset( $data['rating'] ) ? $data['rating'] : '';
+		$this->rating = isset( $data['rating'] ) && is_float( $data['rating'] ) ? $data['rating'] : '';
 
 	}
 
@@ -162,8 +162,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_rating_count_from_api( $data ) {
 
-		// Google Places API does not include rating count.
-		$this->rating_count = '';
+		$this->rating = isset( $data['rating_count'] ) && is_int( $data['rating_count'] ) ? $data['rating_count'] : '';
 
 	}
 
@@ -176,7 +175,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_phone_from_api( $data ) {
 
-		$this->phone = isset( $data['formatted_phone_number'] ) ? $data['formatted_phone_number'] : '';
+		$this->phone = isset( $data['formatted_phone_number'] ) ? sanitize_text_field( $data['formatted_phone_number'] ) : '';
 
 	}
 
@@ -189,7 +188,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_latitude_from_api( $data ) {
 
-		$this->latitude = isset( $data['geometry']['location']['lat'] ) ? $data['geometry']['location']['lat'] : '';
+		$this->latitude = isset( $data['geometry']['location']['lat'] ) && is_float( $data['geometry']['location']['lat'] ) ? $data['geometry']['location']['lat'] : '';
 
 	}
 
@@ -202,7 +201,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_longitude_from_api( $data ) {
 
-		$this->longitude = isset( $data['geometry']['location']['lng'] ) ? $data['geometry']['location']['lng'] : '';
+		$this->longitude = isset( $data['geometry']['location']['lng'] ) && is_float( $data['geometry']['location']['lng'] ) ? $data['geometry']['location']['lng'] : '';
 
 	}
 
@@ -215,7 +214,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_image_url_from_api( $data ) {
 
-		$photoreference = isset( $data['photos'][0]['photo_reference'] ) ? $data['photos'][0]['photo_reference'] : '';
+		$photoreference = isset( $data['photos'][0]['photo_reference'] ) ? sanitize_text_field( $data['photos'][0]['photo_reference'] ) : '';
 
 		if ( ! empty( $photoreference ) ) {
 
@@ -244,7 +243,9 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 		$route         = isset( $address_components['route'] ) ? $address_components['route'] : '';
 		$subpremise    = isset( $address_components['subpremise'] ) ? ' #' . $address_components['subpremise'] : '';
 
-		$this->street_address = $street_number . $route . $subpremise;
+		$street_address = $street_number . $route . $subpremise;
+
+		$this->street_address = isset( $street_address ) ? sanitize_text_field( $street_address ) : '';
 
 	}
 
@@ -257,7 +258,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_city_from_api( $address_components ) {
 
-		$this->city = isset( $address_components['city'] ) ? $address_components['city'] : '';
+		$this->city = isset( $address_components['city'] ) ? sanitize_text_field( $address_components['city'] ) : '';
 
 	}
 
@@ -270,7 +271,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_state_province_from_api( $address_components ) {
 
-		$this->state_province = isset( $address_components['state_province'] ) ? $address_components['state_province'] : '';
+		$this->state_province = isset( $address_components['state_province'] ) ? sanitize_text_field( $address_components['state_province'] ) : '';
 
 	}
 
@@ -283,7 +284,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_postal_code_from_api( $address_components ) {
 
-		$this->postal_code = isset( $address_components['postal_code'] ) ? $address_components['postal_code'] : '';
+		$this->postal_code = isset( $address_components['postal_code'] ) ? sanitize_text_field( $address_components['postal_code'] ) : '';
 
 	}
 
@@ -296,7 +297,7 @@ class WPBR_Google_Places_Business extends WPBR_Business {
 	 */
 	protected function set_country_from_api( $address_components ) {
 
-		$this->country = isset( $address_components['country'] ) ? $address_components['country'] : '';
+		$this->country = isset( $address_components['country'] ) ? sanitize_text_field( $address_components['country'] ) : '';
 
 	}
 
