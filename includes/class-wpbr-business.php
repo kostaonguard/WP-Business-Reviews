afd5a6c5337d25ba742161e1aa5eef7641be7c79
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-05-18 03:56:46 -0400

    Fix alignment

--- includes/class-wpbr-business.php
+++ includes/class-wpbr-business.php
@@ -198,7 +198,7 @@ abstract class WPBR_Business {
 		} else {
 
 			// Request business data from API.
-			$request  = WPBR_Request_Factory::create( $this->business_id, $this->platform );
+			$request       = WPBR_Request_Factory::create( $this->business_id, $this->platform );
 			$business_data = $request->request_business();
 
 			if ( ! is_wp_error( $business_data ) ) {
