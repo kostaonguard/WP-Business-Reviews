920a96fc9664a622479f37f349c938a67a532d4d
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-05-22 03:05:15 -0400

    Move YP data standardization to request class

--- includes/class-wpbr-yp-request.php
+++ includes/class-wpbr-yp-request.php
@@ -62,11 +62,9 @@ class WPBR_YP_Request extends WPBR_Request {
 	 * @param string $business_id ID of the business passed in the API request.
 	 */
 	public function __construct( $business_id ) {
-
 		$this->business_id = $business_id;
 		// TODO: Get API key from database instead of using constant.
 		$this->api_key     = YP_API_KEY;
-
 	}
 
 	/**
@@ -74,10 +72,9 @@ class WPBR_YP_Request extends WPBR_Request {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return WP_Error|array Business data or WP_Error on failure.
+	 * @return array|WP_Error Business data or WP_Error on failure.
 	 */
 	public function request_business() {
-
 		// Define URL parameters of the request URL.
 		$url_params = array(
 
@@ -90,15 +87,7 @@ class WPBR_YP_Request extends WPBR_Request {
 		// Request data from remote API.
 		$response = $this->request( $this->business_path, $url_params );
 
-		if ( is_wp_error( $response ) ) {
-
-			return $response;
-
-		}
-
-		// Return only the relevant portion of the response.
-		return $response['listingsDetailsResult']['listingsDetails']['listingDetail'][0];
-
+		return $response;
 	}
 
 	/**
@@ -109,9 +98,110 @@ class WPBR_YP_Request extends WPBR_Request {
 	 * @return WP_Error|array Reviews data or WP_Error on failure.
 	 */
 	public function request_reviews() {
-
 		// TODO: Define how reviews are requested.
+	}
 
+	/**
+	 * Standardize business properties.
+	 *
+	 * @since 1.0.0
+	 * @see WPBR_Business
+	 *
+	 * @param array $response Business data from remote API.
+	 *
+	 * @return array Standardized business properties.
+	 */
+	public function standardize_business_properties( $response ) {
+		// Drill down to the relevant portion of the response.
+		$r = $response['listingsDetailsResult']['listingsDetails']['listingDetail'][0];
+
+		// Set defaults.
+		$properties = array(
+			'platform'       => $this->platform,
+			'business_id'    => $this->business_id,
+			'business_name'  => null,
+			'page_url'       => null,
+			'image_url'      => null,
+			'rating'         => null,
+			'rating_count'   => null,
+			'phone'          => null,
+			'street_address' => null,
+			'city'           => null,
+			'state_province' => null,
+			'postal_code'    => null,
+			'country'        => null,
+			'latitude'       => null,
+			'longitude'      => null,
+		);
+
+		// Set business name.
+		if ( isset( $r['businessName'] ) ) {
+			$properties['business_name'] = sanitize_text_field( $r['businessName'] );
+		}
+
+		// Set page URL.
+		if ( isset( $r['moreInfoURL'] ) ) {
+			$properties['page_url'] = sanitize_text_field( $r['moreInfoURL'] );
+		}
+
+		// Set rating.
+		if (
+			isset( $r['averageRating'] )
+			&& is_numeric( $r['averageRating'] )
+		) {
+			$properties['rating'] = $r['averageRating'];
+		}
+
+		// Set rating count.
+		if (
+			isset( $r['ratingCount'] )
+			&& is_numeric( $r['ratingCount'] )
+		) {
+			$properties['rating_count'] = $r['ratingCount'];
+		}
+
+		// Set phone.
+		if ( isset( $r['phone'] ) ) {
+			$properties['phone'] = sanitize_text_field( $r['phone'] );
+		}
+
+		// Set street address.
+		if ( isset( $r['street'] ) ) {
+			$properties['street_address'] = sanitize_text_field( $r['street'] );
+		}
+
+		// Set city.
+		if ( isset( $r['city'] ) ) {
+			$properties['city'] = sanitize_text_field( $r['city'] );
+		}
+
+		// Set state.
+		if ( isset( $r['state'] ) ) {
+			$properties['state_province'] = sanitize_text_field( $r['state'] );
+		}
+
+		// Set postal code.
+		if ( isset( $r['zip'] ) ) {
+			$properties['postal_code'] = sanitize_text_field( $r['zip'] );
+		}
+
+		// Set latitude.
+		if (
+			isset( $r['latitude'] )
+			&& is_float( $r['latitude'] )
+		) {
+			$properties['latitude'] = sanitize_text_field( $r['latitude'] );
+		}
+
+		// Set longitude.
+		if (
+			isset( $r['longitude'] )
+			&& is_float( $r['longitude'] )
+		) {
+			$properties['longitude'] = sanitize_text_field( $r['longitude'] );
+		}
+
+		return $properties;
 	}
 
 }
