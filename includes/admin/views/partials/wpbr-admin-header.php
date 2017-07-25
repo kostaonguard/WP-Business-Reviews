4ff6b30cabd1b3b8af5562906e2a2e27473c560a
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-07-25 11:24:33 -0400

    Style admin header

--- includes/admin/views/partials/wpbr-admin-header.php
+++ includes/admin/views/partials/wpbr-admin-header.php
@@ -1,1 +1,3 @@
-<h1>Placeholder for Admin Header</h1>
+<div class="wpbr-admin-header">
+	<img class="wpbr-admin-header__logo" src="<?php esc_attr_e( WPBR_ASSETS_URL . 'images/wpbr-logo-white-wordmark.png' ) ?>" alt="">
+</div>
