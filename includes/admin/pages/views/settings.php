c39f59cc1d9a5563ef18b413a20c9edb17d698c8
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-07-27 15:41:56 -0400

    Add settings view

--- includes/admin/pages/views/settings.php
+++ includes/admin/pages/views/settings.php
@@ -1,14 +1,62 @@
-<?php echo '<pre>'; print_r( $this->settings ); echo '</pre>'; ?>
-
 <!-- Tabs -->
+<ul class="wpbr-nav-tabs">
+	<?php foreach ( $settings as $tab ) : ?>
+		<li><a href=""><?php echo esc_html( $tab['name'] ) ?></a></li>
+	<?php endforeach; ?>
+</ul>
+
+<!-- Panel -->
 <div class="wpbr-admin-page">
-	<!-- Tab Panels -->
-	<div class="wpbr-panel js-wpbr-panel">
-		<div class="wpbr-panel__main">
-			Section Fields
+	<?php foreach ( $settings as $tab ) : ?>
+		<div class="wpbr-panel js-wpbr-panel">
+			<?php
+			// Do not render if no sections.
+			if ( empty( $tab['sections'] ) ) {
+				continue;
+			}
+			?>
+
+			<!-- Main -->
+			<div class="wpbr-panel__main">
+				<?php foreach ( $tab['sections'] as $section ) : ?>
+					<div class="wpbr-admin-section js-wpbr-admin-section">
+						<div class="wpbr-admin-section__header">
+
+							<?php if ( ! empty( $section['heading'] ) ) : ?>
+								<h2 class="wpbr-admin-section__heading"><?php echo esc_html( $section['heading'] ); ?></h2>
+							<?php endif; ?>
+
+							<?php if ( ! empty( $section['desc'] ) ) : ?>
+								<p class="wpbr-admin-section__description"><?php echo wp_kses_post( $section['desc'] ); ?></p>
+							<?php endif; ?>
+
+							<form method="post">
+								<?php foreach ( $section['fields'] as $field ) : ?>
+									<?php if ( ! empty( $field['name'] ) ) : ?>
+										<label><?php echo $field['name'] . ' (' . $field['type'] . ')'; ?></label><br>
+									<?php endif; ?>
+								<?php endforeach; ?>
+							</form>
+
+						</div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+
+			<?php
+			// Show sidebar navigation if more than one section exists.
+			if ( count( $tab['sections'] ) > 1 ) :
+			?>
+				<!-- Sidebar -->
+				<div class="wpbr-panel__sidebar">
+					<ul class="wpbr-nav-sections">
+						<?php foreach ( $tab['sections'] as $section ) : ?>
+							<li><a href=""><?php echo esc_html( $section['name'] ) ?></a></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
+
 		</div>
-		<div class="wpbr-panel__sidebar">
-			Section Navigation
-		</div>
-	</div>
+	<?php endforeach; ?>
 </div>
