c47e215279b914d8b932cef1cbc7c54e202e1c39
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-07-31 13:07:21 -0400

    Remove platform status label as there is no input

--- includes/admin/settings/views/field-platform-status.php
+++ includes/admin/settings/views/field-platform-status.php
@@ -1,8 +1,6 @@
 <tr class="wpbr-settings-field">
 	<th scope="row">
-		<label class="wpbr-settings-field__label" for="<?php echo esc_attr( $field['id'] ); ?>">
-			<?php echo esc_html( $field['name'] ); ?>
-		</label>
+		<?php echo esc_html( $field['name'] ); ?>
 	</th>
 	<td>
 		<strong class="wpbr-platform-status"><?php echo esc_html( 'Not Connected' ); ?></strong>
