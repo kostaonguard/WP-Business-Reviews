5726f5f1c883553c4f92e1661df0ffea9f8ca203
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-09-18 15:22:15 -0400

    Add style minification to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,54 +8,54 @@ var sourcemaps = require( 'gulp-sourcemaps' );
 var autoprefixer = require( 'autoprefixer' );
 var cssnano = require( 'cssnano' );
 var imagemin = require( 'gulp-imagemin' );
+var rename = require( 'gulp-rename' );
+var cleancss = require( 'gulp-clean-css' );
 
-gulp.task( 'styles', function () {
-	var plugins = [
-		autoprefixer( {
-			browsers: [ 'last 2 versions' ],
-		} ),
-		cssnano(),
-	];
-
+gulp.task( 'styles', function() {
 	return gulp.src( 'assets/css/src/**/*.scss' )
-	           .pipe( sass().on( 'error', sass.logError ) )
-	           .pipe( sourcemaps.init() )
-	           .pipe( postcss( plugins ) )
-	           .pipe( sourcemaps.write( '.' ) )
-	           .pipe( gulp.dest( 'assets/css' ) )
-	           .pipe( browsersync.stream() );
-} );
+		.pipe( sourcemaps.init() )
+		.pipe( sass({outputStyle: 'expanded'}).on( 'error', sass.logError ) )
+        .pipe( postcss([ autoprefixer() ]) )
+		.pipe( gulp.dest( 'assets/css' ) )
+		.pipe( rename({ suffix: '.min' }) )
+		.pipe( cleancss() )
+		.pipe( sourcemaps.write( './maps' ) )
+		.pipe( gulp.dest( 'assets/css' ) )
+		.pipe( browsersync.stream() );
+});
 
-gulp.task( 'scripts', function () {
-//	var scripts = [
-//		'assets/js/src/script1.js',
-//	];
+gulp.task( 'scripts', function() {
+
+	//	var scripts = [
+	//		'assets/js/src/script1.js',
+	//	];
 
 	return gulp.src( 'assets/js/src/**/*.js' )
-//	           .pipe( concat( 'main.js' ) )
-	           .pipe( uglify() )
-	           .pipe( gulp.dest( 'assets/js' ) )
-	           .pipe( browsersync.reload( { stream: true } ) );
-} );
 
-gulp.task( 'images', function () {
+		//	           .pipe( concat( 'main.js' ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'assets/js' ) )
+		.pipe( browsersync.reload({ stream: true }) );
+});
+
+gulp.task( 'images', function() {
 	return gulp.src( 'assets/images/src/**/*.+(png|jpg|jpeg|gif|svg)' )
-	           .pipe( imagemin() )
-	           .pipe( gulp.dest( 'assets/images' ) )
-	           .pipe( browsersync.reload( { stream: true } ) );
-} );
+		.pipe( imagemin() )
+		.pipe( gulp.dest( 'assets/images' ) )
+		.pipe( browsersync.reload({ stream: true }) );
+});
 
-gulp.task( 'browsersync', function () {
-	browsersync.init( {
-		proxy: 'wpbr-development.dev',
-	} )
-} );
+gulp.task( 'browsersync', function() {
+	browsersync.init({
+		proxy: 'wpbr-development.dev'
+	});
+});
 
-gulp.task( 'watch', [ 'browsersync', 'styles', 'scripts' ], function () {
+gulp.task( 'watch', [ 'browsersync', 'styles', 'scripts' ], function() {
 	gulp.watch( '**/*.php', browsersync.reload );
-	gulp.watch( 'assets/css/src/**/*.scss', [ 'styles' ] );
-	gulp.watch( 'assets/js/src/**/*.js', [ 'scripts' ] );
-	gulp.watch( 'assets/images/src/**/*.+(png|jpg|jpeg|gif|svg)', [ 'images' ] );
-} );
+	gulp.watch( 'assets/css/src/**/*.scss', [ 'styles' ]);
+	gulp.watch( 'assets/js/src/**/*.js', [ 'scripts' ]);
+	gulp.watch( 'assets/images/src/**/*.+(png|jpg|jpeg|gif|svg)', [ 'images' ]);
+});
 
-gulp.task( 'default', [ 'styles', 'scripts', 'images' ] );
+gulp.task( 'default', [ 'styles', 'scripts', 'images' ]);
