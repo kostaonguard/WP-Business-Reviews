f7570ad8cbbb2702b08c1f98c9bd279d9155aa64
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-10-02 09:34:22 -0400

    Clean up Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,21 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const inProduction = (process.env.NODE_ENV === 'production');
-const DashboardPlugin = require('webpack-dashboard/plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const SuppressChunksPlugin = require('suppress-chunks-webpack-plugin').default;
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-// Sass plugin.
-// const extractSass = new ExtractTextPlugin('css/[name].css');
-const extractSass = new ExtractTextPlugin((inProduction ? '[name].min.css' : '[name].css'));
-const extractSassConfig = {
-	use: [{
-		loader: 'css-loader',
-		options: {
-			sourceMap: true,
-			url: false
-		}
-	}, {
-		loader: 'sass-loader',
-		options: {
-			sourceMap: true,
-			outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested'
-		}
-	}]
-};
-
-const entry = {
-	'js/admin-main': './assets/src/js/admin-main.js',
-	'css/admin-main': './assets/src/css/admin-main.scss'
-}
+const webpack = require( 'webpack' );
+const path = require( 'path' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const inProduction = ( 'production' === process.env.NODE_ENV );
+const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const ImageminPlugin = require( 'imagemin-webpack-plugin' ).default;
+const SuppressChunksPlugin = require( 'suppress-chunks-webpack-plugin' ).default;
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 
 // Webpack config.
 const config = {
-	entry: entry,
+	entry: {
+		'admin-main': './assets/src/js/admin-main.js'
+	},
 	output: {
-		path: path.resolve(__dirname, './assets/dist/'),
-		filename: (inProduction ? '[name].min.js' : '[name].js')
+		path: path.resolve( __dirname, './assets/dist/' ),
+		filename: ( inProduction ? 'js/[name].min.js' : 'js/[name].js' )
 	},
 	devtool: 'source-map',
 	module: {
@@ -50,40 +27,42 @@ const config = {
 			},
 			{
 				test: /\.scss$/,
-				use: extractSass.extract(extractSassConfig)
+				use: ExtractTextPlugin.extract({
+					use: [ {
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							url: false
+						}
+					}, {
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+							outputStyle: 'production' === process.env.NODE_ENV ? 'compressed' : 'nested'
+						}
+					} ]
+				})
 			}
 		]
 	},
 	plugins: [
-		extractSass,
-		new SuppressChunksPlugin(
-			Object.keys(entry).filter(name => (name.indexOf('js') === -1) ),
-			{filter: /\.js(\.map)?$/}
-		),
-		new CleanWebpackPlugin(['assets/dist']),
-		new CopyWebpackPlugin([{
-			from: './assets/src/images/',
-			to: 'images'
-		}]),
+		new ExtractTextPlugin( ( inProduction ? 'css/[name].min.css' : 'css/[name].css' ) ),
+		new CleanWebpackPlugin([ 'assets/dist' ]),
 		new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
 		new BrowserSyncPlugin({
-			// Browse to http://localhost:3000/ during development.
 			files: [
-				'**/*.php',
+				'**/*.php'
 			],
 			host: 'localhost',
 			port: 3000,
-			proxy: 'wpbr-development.dev',
-		}),
-		new DashboardPlugin()
+			proxy: 'wpbr-development.dev'
+		})
 	]
 };
 
-switch (process.env.NODE_ENV) {
-	case 'production':
-		config.plugins.push(new webpack.optimize.UglifyJsPlugin()); // Uglify JS.
-		config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true })); // Minify CSS.
-		break;
+if ( 'production' === process.env.NODE_ENV ) {
+	config.plugins.push( new webpack.optimize.UglifyJsPlugin() ); // Uglify JS.
+	config.plugins.push( new webpack.LoaderOptionsPlugin({ minimize: true }) ); // Minify CSS.
 }
 
 module.exports = config;
