efbe3132ac62b11f33d3a17f5f5be5df1ef5c823
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-10-12 22:47:12 -0400

    Add temporary static view for reviews builder

--- includes/admin/builder/views/builder-controls.php
+++ includes/admin/builder/views/builder-controls.php
@@ -1,2 +1,131 @@
-<?php
-echo 'rendering builder controls';
+<div class="wpbr-builder__controls">
+	<div class="wpbr-builder__section">
+		<div class="wpbr-builder__section-header js-wpbr-section-header">
+			<button class="wpbr-builder__toggle"  aria-expanded="true">
+				<span class="screen-reader-text">Toggle section: Presentation</span>
+				<span class="wpbr-builder__toggle-indicator js-wpbr-toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-down"></span></span>
+			</button>
+			<h3 class="wpbr-builder__section-heading">Presentation Options</h3>
+		</div>
+		<div class="wpbr-builder__section-body js-wpbr-section-body">
+			<div class="wpbr-field">
+				<label class="wpbr-builder__label" for="">Format</label>
+				<select class="wpbr-builder__select js-wpbr-builder-format" name="" id="">
+					<option value="reviews-gallery">Gallery</option>
+					<option value="reviews-list">List</option>
+				</select>
+			</div>
+			<div class="wpbr-field js-wpbr-builder-max-columns-field" style="padding-bottom: 10px;">
+				<label class="wpbr-builder__label" for="">Maximum Columns</label>
+				<input class="js-wpbr-builder-max-columns" type="range" list="tickmarks" min="1" max="6" style="width: 100%;" value="3">
+				<datalist id="tickmarks">
+					<option value="1">
+					<option value="2">
+					<option value="3">
+					<option value="4">
+					<option value="5">
+					<option value="6">
+				</datalist>
+			</div>
+			<div class="wpbr-field">
+				<label class="wpbr-builder__label" for="">Theme</label>
+				<select class="wpbr-builder__select js-wpbr-builder-theme" name="wpbr-theme" id="wpbr-theme">
+					<option value="card">Card</option>
+					<option value="seamless-light">Seamless</option>
+					<option value="seamless-dark">Seamless Dark</option>
+				</select>
+			</div>
+		</div>
+	</div>
+	<div class="wpbr-builder__section">
+		<div class="wpbr-builder__section-header js-wpbr-section-header">
+			<button class="wpbr-builder__toggle"  aria-expanded="true">
+				<span class="screen-reader-text">Toggle section: Business Options</span>
+				<span class="wpbr-builder__toggle-indicator js-wpbr-toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-down"></span></span>
+			</button>
+			<h3 class="wpbr-builder__section-heading">Business Options</h3>
+		</div>
+		<div class="wpbr-builder__section-body js-wpbr-section-business">
+			<div class="wpbr-field js-wpbr-field-platform">
+				<label class="wpbr-builder__label js-wpbr-label" for="">Platform</label>
+				<select class="wpbr-builder__select js-wpbr-control" name="" id="">
+					<option value="">-- Select a Platform --</option>
+					<option value="google" selected>Google</option>
+					<option value="facebook">Facebook</option>
+					<option value="yelp">Yelp</option>
+					<option value="yp">YP</option>
+				</select>
+			</div>
+			<div class="wpbr-field js-wpbr-field-search">
+				<label class="wpbr-builder__label js-wpbr-label" for="">Business</label>
+				<div class="wpbr-builder__control wpbr-builder__search js-wpbr-control">
+					<input class="wpbr-builder__input wpbr-builder__input--search js-wpbr-search-input" type="text" placeholder="Business Name, Location">
+					<button class="button js-wpbr-search-button"><?php esc_html_e( 'Search', 'wpbr' ); ?></button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wpbr-builder__section">
+		<div class="wpbr-builder__section-header js-wpbr-section-header">
+			<button class="wpbr-builder__toggle"  aria-expanded="true">
+				<span class="screen-reader-text">Toggle section: Review Options</span>
+				<span class="wpbr-builder__toggle-indicator js-wpbr-toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-down"></span></span>
+			</button>
+			<h3 class="wpbr-builder__section-heading">Review Options</h3>
+		</div>
+		<div class="wpbr-builder__section-body js-wpbr-section-body">
+<!--			<div class="wpbr-field">-->
+<!--				<label class="wpbr-builder__label" for="">Minimum Star Rating</label>-->
+<!--				<select class="wpbr-builder__select js-wpbr-control-star-rating" name="" id="">-->
+<!--					<option value="5">5 Stars</option>-->
+<!--					<option value="4">4 Stars</option>-->
+<!--					<option value="3">3 Stars</option>-->
+<!--					<option value="2">2 Stars</option>-->
+<!--					<option value="1">1 Stars</option>-->
+<!--				</select>-->
+<!--			</div>-->
+			<div class="wpbr-field">
+				<label class="wpbr-builder__label" for="">Review Order</label>
+				<select class="wpbr-builder__select js-wpbr-control-order" name="wpbr-theme" id="wpbr-theme">
+					<option value="desc">Newest to Oldest</option>
+					<option value="asc">Oldest to Newest</option>
+				</select>
+			</div>
+			<div class="wpbr-field">
+				<label class="wpbr-builder__label" for="">Review Components</label>
+				<fieldset class="wpbr-field__fieldset">
+					<legend class="wpbr-field__legend screen-reader-text">Review Components</legend>
+					<ul class="wpbr-field__options">
+						<li class="wpbr-field__option js-wpbr-field-reviewer-image-vis">
+							<input id="checkbox1" type="checkbox"
+							       name=""
+							       value="" class="js-wpbr-control-reviewer-image-vis" checked>
+							<label for="checkbox1">
+								Show Reviewer Image
+							</label>
+						</li>
+						<li class="wpbr-field__option js-wpbr-field-star-rating-vis">
+							<input id="checkbox2" type="checkbox"
+							       name=""
+							       value="" class="js-wpbr-control-star-rating-vis" checked>
+							<label for="checkbox2">
+								Show Star Rating
+							</label>
+						</li>
+						<li class="wpbr-field__option js-wpbr-field-timestamp-vis">
+							<input id="checkbox3" type="checkbox"
+							       name=""
+							       value="" class="js-wpbr-control-timestamp-vis" checked>
+							<label for="checkbox3">
+								Show Review Date
+							</label>
+						</li>
+					</ul>
+				</fieldset>
+			</div>
+			<div class="wpbr-field">
+				<button class="button button-primary">Save Template</button>
+			</div>
+		</div>
+	</div>
+</div>
--- includes/admin/builder/views/builder-preview.php
+++ includes/admin/builder/views/builder-preview.php
@@ -1,2 +1,29 @@
-<?php
-echo 'rendering builder preview';
+<div class="wpbr-builder__preview">
+	<div class="wpbr-notice wpbr-notice--warning js-wpbr-builder-notice" style="display: none;"><strong>Note:</strong> The Google API returns a maximum of 5 reviews, however your total number may grow over time as new reviews become available.</div>
+	<div class="wpbr-wrap wpbr-theme--card js-wpbr-wrap">
+		<ul class="wpbr-gallery js-wpbr-reviews-list">
+			<?php for ( $i = 0; $i < 12; $i++ ) : ?>
+				<li class="wpbr-gallery__item wpbr-gallery__item--3 js-wpbr-reviews-item" data-rating="5" data-time-created="">
+					<div class="wpbr-review js-wpbr-review">
+						<div class="wpbr-review__header js-wpbr-review-details">
+							<div class="wpbr-review__image js-wpbr-review-image">
+								<img src="" alt="" class="js-wpbr-review-image-el">
+							</div>
+							<div class="wpbr-review__details">
+								<h3 class="wpbr-review__name js-wpbr-review-name">Firstname Lastname</h3>
+								<div class="js-wpbr-star-rating">
+									<span class="wpbr-stars wpbr-stars--generic wpbr-stars--google wpbr-stars--generic-5" aria-label="Rated 5.0 out of 5"></span>
+									<span>★★★★★</span>
+								</div>
+								<span class="wpbr-review__timestamp js-wpbr-review-timestamp">Last week via Facebook</span>
+							</div>
+						</div>
+						<div class="wpbr-review__body js-wpbr-review-body">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae in laborum laudantium neque omnis optio quasi qui sit voluptatum!</p>
+						</div>
+					</div>
+				</li>
+			<?php endfor; ?>
+		</ul>
+	</div>
+</div>
