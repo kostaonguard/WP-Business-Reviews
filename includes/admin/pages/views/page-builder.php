a4105cf68a08463dfd1c67e55effa976223d5b76
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-10-12 22:46:14 -0400

    Remove wrappers from builder view

--- includes/admin/pages/views/page-builder.php
+++ includes/admin/pages/views/page-builder.php
@@ -1,9 +1,6 @@
 <div class="wpbr-admin-page">
-	<div class="wpbr-builder">
-		<div class="wpbr-builder__controls">
-			<?php $builder_controls->render(); ?>
-		</div>
-		<div class="wpbr-builder__preview">
-			<?php $builder_preview->render(); ?>
-		</div>
+	<div class="wpbr-builder js-wpbr-builder">
+		<?php $builder_controls->render(); ?>
+		<?php $builder_preview->render(); ?>
+	</div>
 </div>
