96d9afadc7903ffa3583ba9ed59251d1c516c7d9
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-10-17 11:28:38 -0400

    Add field classes

---
+++ includes/fields/class-field-factory.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Defines the Field_Factory class
+ *
+ * @package WP_Business_Reviews\Includes\Request
+ * @since   1.0.0
+ */
+
+namespace WP_Business_Reviews\Includes\Fields;
+
+/**
+ * Determines the appropriate Field subclass based on type.
+ *
+ * @since 1.0.0
+ * @see   Request
+ */
+class Field_Factory {
+
+	/**
+	 * Creates a new instance of the Field subclass.
+	 *
+	 * @since 1.0.0
+	 * @see Field
+	 *
+	 * @param array $atts Field attributes.
+	 *
+	 * @return Field|boolean Instance of Field class or false.
+	 */
+	public static function create( array $atts = array() ) {
+		if ( ! isset( $atts['type'] ) ) {
+			return false;
+		}
+
+		switch ( $atts['type'] ) {
+			case 'select':
+				return new Field( $atts );
+		}
+
+	}
+}
---
+++ includes/fields/class-field.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Defines the Field class
+ *
+ * @package WP_Business_Reviews\Includes
+ * @since   1.0.0
+ */
+
+namespace WP_Business_Reviews\Includes\Fields;
+
+use WP_Business_Reviews\Includes\View;
+
+/**
+ * Implements a field based on provided attributes.
+ *
+ * @since 1.0.0
+ */
+class Field {
+
+	/**
+	 * Default view used to render the field.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @var string
+	 */
+	const DEFAULT_VIEW = 'views/field.php';
+
+	/**
+	 * Field attributes.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @var array
+	 */
+	protected $atts;
+
+	/**
+	 * Instantiates the Field object.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $atts {
+	 *     Optional. Field attributes.
+	 *
+	 *     @type string $id           Field ID.
+	 *     @type string $name         Field name also used as label.
+	 *     @type string $type         Field type to determine Field subclass.
+	 *     @type string $tooltip      Tooltip to clarify field purpose.
+	 *     @type string $description  Description to clarify field use.
+	 *     @type string $default      Default field value.
+	 *     @type string $value        Field value.
+	 *     @type array  $control_atts Placeholder value for relevant input types.
+	 *     @type array  $options      Field options for select/radios/checkboxes.
+	 *     @type string $view         View used to render the field.
+	 * }
+	 */
+	public function __construct( array $atts = array() ) {
+		$this->atts = $this->prepare_atts( $atts );
+	}
+
+	/**
+	 * Gets field attributes.
+	 *
+	 * @return array Field attributes.
+	 */
+	public function get_atts() {
+		return $this->atts;
+	}
+
+	/**
+	 * Gets a single field attribute.
+	 *
+	 * @param string $att Key associated with an attribute.
+	 *
+	 * @return string|array Value of the field attribute if it exists.
+	 */
+	public function get_att( $key ) {
+		return isset( $this->atts[ $key ] )	? $this->atts[ $key ] : '';
+	}
+
+	/**
+	 * Merges default field attributes with provided attributes.
+	 *
+	 * @param array $atts Field attributes.
+	 *
+	 * @return array Fully populated field attributes.
+	 */
+	protected function prepare_atts( array $atts ) {
+		$defaults = array(
+			'id'           => '',
+			'name'         => '',
+			'tooltip'      => '',
+			'type'         => '',
+			'default'      => '',
+			'value'        => '',
+			'control_atts' => array(),
+			'description'  => '',
+			'options'      => array(),
+			'view'         => self::DEFAULT_VIEW,
+		);
+
+		return wp_parse_args( $atts, $defaults );
+	}
+
+	/**
+	 * Render a given view.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string|View $view    View to render. Can be a path to a view file
+	 *                             or an instance of a View object.
+	 * @param array|null  $context Optional. Context variables for use in view.
+	 */
+	public function render_view( $view ) {
+		$view_object = is_string( $view ) ? new View( $view ) : $view;
+		$view_object->render( $this->get_atts() );
+	}
+}
