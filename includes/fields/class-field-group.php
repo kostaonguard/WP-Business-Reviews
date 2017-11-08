0e5bfbed71f787cd7af171e00ab625411ec87785
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-07 23:48:41 -0500

    Add getter methods to field group class

--- includes/fields/class-field-group.php
+++ includes/fields/class-field-group.php
@@ -5,9 +5,9 @@ namespace WP_Business_Reviews\Includes\Fields;
 use WP_Business_Reviews\Includes\Fields\Field;
 
 class Field_Group {
-	private $fields;
 	private $group_id;
 	private $group_name;
+	private $fields;
 
 	public function __construct( $group_id, $group_name, array $fields = array() ) {
 		$this->group_id   = $group_id;
@@ -24,11 +24,21 @@ class Field_Group {
 
 	public function add_fields( array $fields ) {
 		foreach ( $fields as $field ) {
-			if ( ! $field instanceof Field ) {
-				continue;
+			if ( $field instanceof Field ) {
+				$this->add_field( $field );
 			}
-
-			$this->add_field( $field );
 		}
+	}
+
+	public function get_group_id() {
+		return $this->group_id;
+	}
+
+	public function get_group_name() {
+		return $this->group_name;
+	}
+
+	public function get_fields() {
+		return $this->fields;
 	}
 }
