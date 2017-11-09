12fc07355593a1807c7ec69b54c78c7df3c78b1f
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-08 23:19:51 -0500

    Update reviews builder controls to use field groups

--- views/reviews-builder-controls.php
+++ views/reviews-builder-controls.php
@@ -1,27 +1,24 @@
 <?php
-use WP_Business_Reviews\Includes\Config;
-use WP_Business_Reviews\Includes\Fields\Field_Factory;
-
-$config = new Config( WPBR_PLUGIN_DIR . 'config/reviews-builder.php' );
+$field_groups = $this->context;
 ?>
 <div id="wpbr-control-panel" class="wpbr-builder__controls">
-	<?php foreach ( $config as $section ) : ?>
-		<div id="wpbr-section<?php echo esc_attr( $section['id'] ); ?>" class="wpbr-builder__section">
+	<?php foreach ( $field_groups as $field_group ) : ?>
+		<div id="wpbr-section<?php echo esc_attr( $field_group->get_group_id() ); ?>" class="wpbr-builder__section">
 			<div class="wpbr-builder__section-header js-wpbr-section-header">
 				<button class="wpbr-builder__toggle"  aria-expanded="true">
-					<span class="screen-reader-text">Toggle section: <?php esc_html_e( $section['name'] ); ?></span>
+					<span class="screen-reader-text">Toggle section: <?php esc_html_e( $field_group->get_group_name() ); ?></span>
 					<span class="wpbr-builder__toggle-indicator js-wpbr-toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-down"></span></span>
 				</button>
-				<h3 class="wpbr-builder__section-heading"><?php esc_html_e( $section['name'] ); ?></h3>
+				<h3 class="wpbr-builder__section-heading"><?php esc_html_e( $field_group->get_group_name() ); ?></h3>
 			</div>
 
 			<div class="wpbr-builder__section-body js-wpbr-section-body">
-				<?php foreach ( $section['fields'] as $atts ) : ?>
-					<?php
-					$field = Field_Factory::create( $atts );
+				<?php
+				$fields = $field_group->get_fields();
+				foreach ( $fields as $field ) {
 					$field->render_view( $field->get_att( 'view' ) );
-					?>
-				<?php endforeach; ?>
+				}
+				?>
 			</div>
 		</div>
 	<?php endforeach; ?>
