cef4486bd2f662b7a78399571506e2ce44d5564b
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-13 15:15:21 -0500

    Remove need for field group class

--- includes/class-reviews-builder.php
+++ includes/class-reviews-builder.php
@@ -29,13 +29,13 @@ class Reviews_Builder {
 	private $config;
 
 	/**
-	 * Array of field group objects that populates the Reviews Builder controls.
+	 * Multidimensional array of field objects.
 	 *
 	 * @since  1.0.0
 	 * @var    array
 	 * @access private
 	 */
-	private $field_groups;
+	private $field_hierarchy;
 
 	/**
 	 * Instantiates a Reviews_Builder object.
@@ -71,7 +71,7 @@ class Reviews_Builder {
 	 */
 	public function init() {
 		// Process the config to create field objects.
-		$this->field_groups = $this->process_config( $this->config );
+		$this->field_hierarchy = $this->process_config( $this->config );
 	}
 
 	/**
@@ -87,40 +87,40 @@ class Reviews_Builder {
 			return array();
 		}
 
-		$field_groups = array();
+		$field_hierarchy = array();
 
-		foreach ( $config as $field_group ) {
-			if ( ! isset( $field_group['id'], $field_group['name'] ) ) {
+		foreach ( $config as $section ) {
+			if ( ! isset( $section['id'], $section['name'] ) ) {
 				// Skip if field group ID or name is not set.
 				continue;
 			}
 
-			// Create new field group based on the config item.
-			$field_group_obj = new Field_Group( $field_group['id'], $field_group['name'] );
+			$field_objects = array();
 
-			// Populate the field group with individual fields.
-			if ( isset( $field_group['fields'] ) ) {
-				foreach ( $field_group['fields'] as $field ) {
-					// Check if the field ID is set.
-					if ( ! isset( $field['id'] ) ) {
-						// Skip if field ID is not set.
+			// Populate the section with individual field objects.
+			if ( isset( $section['fields'] ) ) {
+				foreach ( $section['fields'] as $field_atts ) {
+					if ( ! isset( $field_atts['id'] ) ) {
 						continue;
 					}
 
-					// Create new field.
-					$field_obj = Field_Factory::create_field( $field );
-
 					// TODO: Update value if set.
+					$field_value = null;
 
-					$field_group_obj->add_field( $field_obj );
+					// Add field object to array.
+					$field_objects[] = Field_Factory::create_field( $field_atts, $field_value );
 				}
 			}
 
-			// Add field object to field group.
-			$field_groups[ $field_group['id'] ] = $field_group_obj;
+			// Add section to field hierarchy.
+			$field_hierarchy[ $section['id'] ] = array(
+				'id'   => $section['id'],
+				'name' => $section['name'],
+				'fields' => $field_objects,
+			);
 		}
 
-		return $field_groups;
+		return $field_hierarchy;
 	}
 
 	/**
@@ -130,6 +130,6 @@ class Reviews_Builder {
 	 */
 	public function render() {
 		$view_object = new View( WPBR_PLUGIN_DIR . 'views/reviews-builder/reviews-builder-main.php' );
-		$view_object->render( $this->field_groups );
+		$view_object->render( $this->field_hierarchy );
 	}
 }
--- views/reviews-builder/reviews-builder-settings.php
+++ views/reviews-builder/reviews-builder-settings.php
@@ -1,21 +1,20 @@
 <?php
-$field_groups = $this->context;
+$sections = $this->context;
 ?>
 <div id="wpbr-reviews-builder-settings" class="wpbr-reviews-builder__settings">
-	<?php foreach ( $field_groups as $field_group ) : ?>
-		<div id="wpbr-section<?php echo esc_attr( $field_group->get_group_id() ); ?>" class="wpbr-reviews-builder__section">
+	<?php foreach ( $sections as $section ) : ?>
+		<div id="wpbr-section<?php echo esc_attr( $section['id'] ); ?>" class="wpbr-reviews-builder__section">
 			<div class="wpbr-reviews-builder__section-header js-wpbr-section-header">
 				<button class="wpbr-reviews-builder__toggle"  aria-expanded="true">
-					<span class="screen-reader-text">Toggle section: <?php esc_html_e( $field_group->get_group_name() ); ?></span>
+					<span class="screen-reader-text">Toggle section: <?php esc_html_e( $section['name'] ); ?></span>
 					<span class="wpbr-reviews-builder__toggle-indicator js-wpbr-toggle-indicator" aria-hidden="true"><span class="dashicons dashicons-arrow-down"></span></span>
 				</button>
-				<h3 class="wpbr-reviews-builder__section-heading"><?php esc_html_e( $field_group->get_group_name() ); ?></h3>
+				<h3 class="wpbr-reviews-builder__section-heading"><?php esc_html_e( $section['name'] ); ?></h3>
 			</div>
 
 			<div class="wpbr-reviews-builder__section-body js-wpbr-section-body">
 				<?php
-				$fields = $field_group->get_fields();
-				foreach ( $fields as $field ) {
+				foreach ( $section['fields'] as $field ) {
 					$field->render_view( WPBR_PLUGIN_DIR . 'views/fields/field-main.php', true );
 				}
 				?>
