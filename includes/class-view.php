53720bf74ac2ba3f3132b0631239cae33e82b978
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-13 12:30:07 -0500

    Update view class to optionally echo output

--- includes/class-view.php
+++ includes/class-view.php
@@ -44,39 +44,63 @@ class View {
 	protected $context = array();
 
 	/**
-	 * Instantiate a View object.
-
-	 * @param string $uri URI of the PHP view to render, relative to the plugin's
-	 *                    root directory (e.g. views/view-name.php).
-	 */
-	public function __construct( $uri ) {
-		$this->uri = $this->validate( $uri );
-	}
-
-	/**
-	 * Render a given URI.
-	 *
-	 * @param array $context Optional. Associative array with context variables.
-	 */
-	public function render( array $context = array() ) {
-		$this->context = $context;
-		include $this->uri;
-	}
-
-	/**
-	 * Render a partial view.
+	 * Instantiates a View object.
 	 *
 	 * @param string $uri URI of the PHP view to render.
-	 * @param array  $context Optional. Associative array with context variables.
 	 */
-	public function render_partial( $uri, array $context = array() ) {
+	public function __construct( $uri ) {
+		$this->uri = $uri;
+	}
+
+	/**
+	 * Renders the associated view.
+	 *
+	 * @param array $context Optional. Associative array with context variables.
+	 * @param bool  $echo    Optional. Whether to echo the output immediately. Defaults to true.
+	 * @return string HTML rendering of the view.
+	 */
+	public function render( $context = null, $echo = true ) {
+		if ( ! $this->validate( $this->uri ) ) {
+			return false;
+		}
+
+		// Set the context for the view.
+		$this->context = $context;
+
+		// Store the view's contents in output buffer.
+		ob_start();
+		include $this->uri;
+		$output = ob_get_clean();
+
+		// Echo or return output.
+		if ( $echo ) {
+			echo $output;
+		} else {
+			return $output;
+		}
+	}
+
+	/**
+	 * Renders a partial view.
+	 *
+	 * @param string $uri     URI of the PHP view to render.
+	 * @param array  $context Optional. Associative array with context variables.
+	 * @param bool   $echo    Optional. Whether to echo the output immediately. Defaults to true.
+	 */
+	public function render_partial( $uri, $context = null, $echo = true ) {
+		if ( ! $this->validate( $this->uri ) ) {
+			return false;
+		}
+
 		if ( empty( $context ) ) {
 			$context = $this->context;
-		};
+		}
 
+		// Create new View object and pass the current context.
 		$view = new static( $uri );
 
-		$view->render( $context );
+		// Render partial.
+		$view->render( $context, $echo );
 	}
 
 	/**
