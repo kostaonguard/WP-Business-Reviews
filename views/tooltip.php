10750c803a38866261d6b70f8e17ac1d5b967b3d
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 14:46:34 -0500

    Improve readability of tooltip attributes

--- views/tooltip.php
+++ views/tooltip.php
@@ -1,1 +1,7 @@
-<span id="wpbr-tooltip-<?php echo esc_attr( $this->id ); ?>" class="wpbr-tooltip wpbr-tooltip--medium wpbr-tooltip--top" aria-label="<?php echo esc_attr( $this->tooltip ); ?>"><span class="dashicons dashicons-editor-help"></span></span>
+<span
+	id="wpbr-tooltip-<?php echo esc_attr( $this->id ); ?>"
+	class="wpbr-tooltip wpbr-tooltip--medium wpbr-tooltip--top"
+	aria-label="<?php echo esc_attr( $this->tooltip ); ?>"
+	>
+	<span class="dashicons dashicons-editor-help"></span>
+</span>
