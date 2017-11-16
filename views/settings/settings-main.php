0040457582908f89644d824b1895c0b2dbb065f8
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-15 21:18:36 -0500

    Restore padding around settings page

--- views/settings/settings-main.php
+++ views/settings/settings-main.php
@@ -9,7 +9,8 @@
 namespace WP_Business_Reviews;
 ?>
 
-<?php
-$this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' );
-$this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-panels.php' );
-?>
+<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
+
+<div class="wpbr-admin-page wpbr-admin-page--pad">
+	<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-panels.php' ); ?>
+</div>
