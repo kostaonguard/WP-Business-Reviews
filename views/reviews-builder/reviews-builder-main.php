cea2d82a890e48af0c52d3fbb00cda1059c56c0b
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-15 22:21:08 -0500

    Remove admin page wrapper from individual views

--- views/reviews-builder/reviews-builder-main.php
+++ views/reviews-builder/reviews-builder-main.php
@@ -1,23 +1,21 @@
-<div class="wpbr-admin-page">
-	<div id="wpbr-reviews-builder" class="wpbr-reviews-builder js-wpbr-reviews-builder">
-		<div class="wpbr-reviews-builder__toolbar">
-			<div id="titlediv" class="wpbr-reviews-builder__title ">
-				<div id="titlewrap">
-					<input type="text" name="post_title" size="30" value="" id="title" spellcheck="true" autocomplete="off" placeholder="Add title">
-				</div>
-			</div>
-			<div class="wpbr-reviews-builder__tools">
-				<ul class="wpbr-inline-list">
-					<li class="wpbr-inline-list__item"><button id="wpbr-control-settings" class="button"><?php esc_html_e( 'Settings', 'wpbr' ); ?></button></li>
-					<li class="wpbr-inline-list__item"><button id="wpbr-control-save" class="button button-primary"><?php esc_html_e( 'Save Review Set', 'wpbr' ); ?></button></li>
-				</ul>
+<div id="wpbr-reviews-builder" class="wpbr-reviews-builder js-wpbr-reviews-builder">
+	<div class="wpbr-reviews-builder__toolbar">
+		<div id="titlediv" class="wpbr-reviews-builder__title ">
+			<div id="titlewrap">
+				<input type="text" name="post_title" size="30" value="" id="title" spellcheck="true" autocomplete="off" placeholder="Add title">
 			</div>
 		</div>
-		<div class="wpbr-reviews-builder__workspace">
-			<?php
-			$this->render_partial( WPBR_PLUGIN_DIR . 'views/reviews-builder/reviews-builder-preview.php' );
-			$this->render_partial( WPBR_PLUGIN_DIR . 'views/reviews-builder/reviews-builder-settings.php' );
-			?>
+		<div class="wpbr-reviews-builder__tools">
+			<ul class="wpbr-inline-list">
+				<li class="wpbr-inline-list__item"><button id="wpbr-control-settings" class="button"><?php esc_html_e( 'Settings', 'wpbr' ); ?></button></li>
+				<li class="wpbr-inline-list__item"><button id="wpbr-control-save" class="button button-primary"><?php esc_html_e( 'Save Review Set', 'wpbr' ); ?></button></li>
+			</ul>
 		</div>
+	</div>
+	<div class="wpbr-reviews-builder__workspace">
+		<?php
+		$this->render_partial( WPBR_PLUGIN_DIR . 'views/reviews-builder/reviews-builder-preview.php' );
+		$this->render_partial( WPBR_PLUGIN_DIR . 'views/reviews-builder/reviews-builder-settings.php' );
+		?>
 	</div>
 </div>
--- views/settings/settings-main.php
+++ views/settings/settings-main.php
@@ -1,16 +1,4 @@
-<?php
-/**
- * Displays the settings UI for the plugin
- *
- * @package WP_Business_Reviews
- * @since 0.1.0
- */
-
-namespace WP_Business_Reviews;
-?>
-
-<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
-
-<div class="wpbr-admin-page wpbr-admin-page--pad">
+<div class="wpbr-settings">
+	<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
 	<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-panels.php' ); ?>
 </div>
