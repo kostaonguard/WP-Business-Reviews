f2bde9755f6d95597d75ec85b70a931ed7f88a0b
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 13:18:49 -0500

    Set submit_button() wrapper to false

--- views/field/controls/control-save.php
+++ views/field/controls/control-save.php
@@ -1,6 +1,11 @@
 <div class="wpbr-field wpbr-field--setting">
 	<?php
 	wp_nonce_field( 'wpbr_settings_save', 'wpbr_settings_nonce' );
-	submit_button();
+	submit_button(
+		__( 'Save Changes', 'wpbr' ),
+		'primary',
+		$this->id,
+		false
+	);
 	?>
 </div>
