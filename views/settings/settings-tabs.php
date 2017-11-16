b8fb5f8ea415ca8ba49ed9b26ac7cd864e091880
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-15 20:28:54 -0500

    Remove empty checks

--- views/settings/settings-tabs.php
+++ views/settings/settings-tabs.php
@@ -2,18 +2,14 @@
 <nav>
 	<ul class="wpbr-tabs js-wpbr-tabs">
 		<?php foreach ( $this->field_hierarchy as $tab ) : ?>
-			<?php
-			$tab_id   = ! empty( $tab['id'] ) ? $tab['id'] : '';
-			$tab_name = ! empty( $tab['name'] ) ? $tab['name'] : '';
-			?>
 			<li class="wpbr-tabs__item">
 				<a
-					id="wpbr-tab-<?php echo esc_attr( $tab_id ); ?>"
+					id="wpbr-tab-<?php echo esc_attr( $tab['id'] ); ?>"
 					class="wpbr-tabs__link js-wpbr-tab"
-					href="#wpbr-panel-<?php echo esc_attr( $tab_id ); ?>"
-					data-tab-id="<?php echo esc_attr( $tab_id ); ?>
+					href="#wpbr-panel-<?php echo esc_attr( $tab['id'] ); ?>"
+					data-tab-id="<?php echo esc_attr( $tab['id'] ); ?>
 					">
-					<?php echo esc_html( $tab_name ); ?>
+					<?php echo esc_html( $tab['name'] ); ?>
 				</a>
 			</li>
 		<?php endforeach; ?>
