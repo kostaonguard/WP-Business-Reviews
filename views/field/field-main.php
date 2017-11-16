3feae38e19581276ba2a3900e3914e5afc0a3ab3
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 14:02:16 -0500

    Only display field name partial if name is set

--- views/field/field-main.php
+++ views/field/field-main.php
@@ -1,11 +1,13 @@
 <div id="wpbr-field-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field js-wpbr-field">
 	<?php
-	if ( 'label' === $this->name_element ) {
-		// Render field name in label tag.
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-label.php' );
-	} else {
-		// Render field name in span tag.
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-name.php' );
+	if ( ! empty( $this->name ) ) {
+		if ( ! empty( $this->name_element ) && 'label' === $this->name_element ) {
+			// Render field name in label tag.
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-label.php' );
+		} else {
+			// Render field name in span tag (intended for use alongside fieldsets).
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-name.php' );
+		}
 	}
 
 	if ( ! empty( $this->tooltip ) ) {
