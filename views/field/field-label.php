9e287c8b8f0b8253434472dda7225f06b8415aec
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 14:00:51 -0500

    Fix for attribute to match control ID

--- views/field/field-label.php
+++ views/field/field-label.php
@@ -1,3 +1,3 @@
-<label id="wpbr-field-name-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__name" for="wpbr-field-control-<?php echo esc_attr( $this->id ); ?>">
+<label id="wpbr-field-name-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__name" for="wpbr-control-<?php echo esc_attr( $this->id ); ?>">
 	<?php esc_html_e( $this->name ); ?>
 </label>
