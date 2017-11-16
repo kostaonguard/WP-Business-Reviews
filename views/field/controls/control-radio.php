717a113d0bbdb7907bdaf2f49aa7d59af7c43c45
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 03:13:44 -0500

    Add functionality to radio control

--- views/field/controls/control-radio.php
+++ views/field/controls/control-radio.php
@@ -1,1 +1,20 @@
-Radio Placeholder
+<fieldset class="wpbr-field__fieldset">
+	<legend class="screen-reader-text"><?php echo esc_html( $this->name ); ?></legend>
+	<ul class="wpbr-field__options">
+		<?php foreach ( $this->options as $option_value => $option_text ) : ?>
+			<li class="wpbr-field__option">
+				<input
+					type="radio"
+					id="wpbr-control-<?php echo esc_attr( $this->id . '-' . $option_value ); ?>"
+					class="wpbr-field__radio"
+					name="<?php echo esc_attr( $this->id ); ?>"
+					value="<?php echo esc_attr( $option_value ); ?>"
+					<?php checked( $option_value, $this->value ); ?>
+					>
+				<label for="wpbr-control-<?php echo esc_attr( $this->id . '-' . $option_value ); ?>">
+					<?php echo esc_html( $option_text ); ?>
+				</label>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+</fieldset>
