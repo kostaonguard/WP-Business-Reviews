d01d43144fed91554c98cd1fa9647af63e80008a
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-15 20:26:41 -0500

    Improve readability of anchor attributes

--- views/settings/settings-tabs.php
+++ views/settings/settings-tabs.php
@@ -7,7 +7,14 @@
 			$tab_name = ! empty( $tab['name'] ) ? $tab['name'] : '';
 			?>
 			<li class="wpbr-tabs__item">
-				<a id="wpbr-tab-<?php echo esc_attr( $tab_id ); ?>" class="wpbr-tabs__link js-wpbr-tab" href="#wpbr-panel-<?php echo esc_attr( $tab_id ); ?>" data-tab-id="<?php echo esc_attr( $tab_id ); ?>"><?php echo esc_html( $tab_name ); ?></a>
+				<a
+					id="wpbr-tab-<?php echo esc_attr( $tab_id ); ?>"
+					class="wpbr-tabs__link js-wpbr-tab"
+					href="#wpbr-panel-<?php echo esc_attr( $tab_id ); ?>"
+					data-tab-id="<?php echo esc_attr( $tab_id ); ?>
+					">
+					<?php echo esc_html( $tab_name ); ?>
+				</a>
 			</li>
 		<?php endforeach; ?>
 	</ul>
