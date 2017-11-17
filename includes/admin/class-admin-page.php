460094e36c0cf6b232dfe94cf244c2302605477e
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-17 15:48:55 -0500

    Remove render method from admin Admin_Page

--- includes/admin/class-admin-page.php
+++ includes/admin/class-admin-page.php
@@ -15,21 +15,22 @@ namespace WP_Business_Reviews\Includes\Admin;
  * @see Admin_Menu
  */
 class Admin_Page {
+	// TODO: Document properties.
+
 	/**
 	 * Instantiates a Submenu_Page object.
 	 *
-	 * @param string   $parent_slug The slug name for the parent menu (or the file name of a standard
-	 *                              WordPress admin page).
-	 * @param string   $page_title  The text to be displayed in the title tags of the page when the menu
-	 *                              is selected.
-	 * @param string   $menu_title  The text to be used for the menu.
-	 * @param string   $capability  The capability required for this menu to be displayed to the user.
-	 * @param string   $menu_slug   The slug name to refer to this menu by. Should be unique for this menu
-	 *                              and only include lowercase alphanumeric, dashes, and underscores characters
-	 *                              to be compatible with sanitize_key().
-	 * @param callable $function    The function to be called to output the content for this page.
+	 * @param string $parent_slug The slug name for the parent menu (or the file name of a standard
+	 *                            WordPress admin page).
+	 * @param string $page_title  The text to be displayed in the title tags of the page when the menu
+	 *                            is selected.
+	 * @param string $menu_title  The text to be used for the menu.
+	 * @param string $capability  The capability required for this menu to be displayed to the user.
+	 * @param string $menu_slug   The slug name to refer to this menu by. Should be unique for this menu
+	 *                            and only include lowercase alphanumeric, dashes, and underscores characters
+	 *                            to be compatible with sanitize_key().
 	 */
-	public function __construct( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function = '' ) {
+	public function __construct( $parent_slug, $page_title, $menu_title, $capability, $menu_slug ) {
 		$this->parent_slug = $parent_slug;
 		$this->page_title  = $page_title;
 		$this->menu_title  = $menu_title;
@@ -43,37 +44,8 @@ class Admin_Page {
 			$this->page_title,
 			$this->menu_title,
 			$this->capability,
-			$this->menu_slug,
-			array( $this, 'render_page' )
+			$this->menu_slug
+			// TODO: Add default callable to ensure the page renders.
 		);
-	}
-
-	/**
-	 * Renders a given view.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string|View $view View to render. Can be a path to a view file
-	 *                          or an instance of a View object.
-	 */
-	public function render_view( $view ) {
-		$view_object = is_string( $view ) ? new View( $view ) : $view;
-		$view_object->render();
-	}
-
-	/**
-	 * Renders the admin page.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string|View $view View to render. Can be a path to a view file
-	 *                          or an instance of a View object.
-	 */
-	public function render_page( $view = '' ) {
-		// TODO: Add hook before admin page view.
-		if ( ! empty( $view ) ) {
-			$this->render_view( $view );
-		}
-		// TODO: Add hook after admin page view.
 	}
 }
