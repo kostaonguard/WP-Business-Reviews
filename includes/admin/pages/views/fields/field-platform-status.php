3f909920563a043d520e31aa64a9c7f2c9cf67b4
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-17 10:49:10 -0500

    Remove old fields that have been converted for use with config

--- includes/admin/pages/views/fields/field-platform-status.php
+++
@@ -1,9 +0,0 @@
-<div class="wpbr-field wpbr-field--platform-status<?php echo ! empty( $class ) ? ' ' . esc_attr( $class ) : ''; ?>">
-	<span class="wpbr-field__name"><?php echo esc_html( $field['name'] ); ?></span>
-	<div class="wpbr-field__control">
-		<strong class="wpbr-platform-status"><?php echo esc_html( 'Not Connected' ); ?></strong>
-	</div>
-	<?php if ( ! empty( $field['desc'] ) ) : ?>
-		<p class="wpbr-field__description"><?php $this->render_field_description( $field['desc'] ); ?></p>
-	<?php endif; ?>
-</div>
