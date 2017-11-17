53c408b87b861df9ab21f6930930caafd404580d
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-17 09:30:35 -0500

    Add flex wrapper to search field

--- views/field/controls/control-search.php
+++ views/field/controls/control-search.php
@@ -1,16 +1,18 @@
-<input
-	type="text"
-	id="wpbr-control-<?php echo esc_attr( $this->id ); ?>"
-	class="wpbr-field__control js-wpbr-search-input"
-	name="<?php echo esc_attr( $this->id ); ?>"
-	value="<?php echo esc_attr( $this->value ); ?>"
-	<?php
-	// Output any additional control attributes.
-	if ( ! empty( $this->control_atts ) ) {
-		foreach ( $this->control_atts as $name => $value ) {
-			echo ' ' . esc_attr( $name ) . '="' . esc_attr( $value ) . '"';
+<div class="wpbr-field__flex">
+	<input
+		type="text"
+		id="wpbr-control-<?php echo esc_attr( $this->id ); ?>"
+		class="wpbr-field__input js-wpbr-search-input"
+		name="<?php echo esc_attr( $this->id ); ?>"
+		value="<?php echo esc_attr( $this->value ); ?>"
+		<?php
+		// Output any additional control attributes.
+		if ( ! empty( $this->control_atts ) ) {
+			foreach ( $this->control_atts as $name => $value ) {
+				echo ' ' . esc_attr( $name ) . '="' . esc_attr( $value ) . '"';
+			}
 		}
-	}
-	?>
-	>
-<button class="button wpbr-field__inline-button js-wpbr-search-button"><?php esc_html_e( 'Search', 'wpbr' ); ?></button>
+		?>
+		>
+	<button class="button wpbr-field__inline-button js-wpbr-search-button"><?php esc_html_e( 'Search', 'wpbr' ); ?></button>
+</div>
