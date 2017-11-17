a24686fddc190aa2f3610d744744320a04bd0174
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-16 19:39:13 -0500

    Add settings component to restore padding around panels

--- views/settings/settings-main.php
+++ views/settings/settings-main.php
@@ -1,4 +1,8 @@
 <div class="wpbr-settings">
-	<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
-	<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-panels.php' ); ?>
+	<div class="wpbr-settings__tabs">
+		<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
+	</div>
+	<div class="wpbr-settings__panels">
+		<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-panels.php' ); ?>
+	</div>
 </div>
