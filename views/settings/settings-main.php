71ce461f3c912a396db798ec1068a15f28775367
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-17 09:50:33 -0500

    Add JS handle to settings root element

--- views/settings/settings-main.php
+++ views/settings/settings-main.php
@@ -1,4 +1,4 @@
-<div class="wpbr-settings">
+<div class="wpbr-settings js-wpbr-settings">
 	<div class="wpbr-settings__tabs">
 		<?php $this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/settings-tabs.php' ); ?>
 	</div>
