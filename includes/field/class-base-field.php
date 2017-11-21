335a697b95f454f546cd4c91e959fc14028838c1
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-21 17:31:17 -0500

    Move default args logic from Field_Factory to Base_Field

--- includes/field/class-base-field.php
+++ includes/field/class-base-field.php
@@ -11,24 +11,47 @@ namespace WP_Business_Reviews\Includes\Field;
 use WP_Business_Reviews\Includes\View;
 
 /**
- * Implements a basic field based on provided attributes.
+ * Implements a basic field based on provided arguments.
  *
  * @since 0.1.0
  */
 class Base_Field {
 	/**
+	 * Unique identifier of the field.
+	 *
+	 * @since 0.1.0
+	 * @var string $id
+	 */
+	protected $id;
+
+	/**
+	 * Field arguments used to define field elements and their attributes.
+	 *
+	 * @since 0.1.0
+	 * @var string $args
+	 */
+	protected $args;
+
+	/**
+	 * Field value.
+	 *
+	 * @since 0.1.0
+	 * @var string $value
+	 */
+	protected $value;
+
+	/**
 	 * Instantiates a Base_Field object.
 	 *
 	 * @since 0.1.0
 	 *
-	 * @param array $atts {
-	 *     Field attributes.
+	 * @param string $id Unique identifier of the field.
+	 * @param array  $args {
+	 *     Field arguments.
 	 *
-	 *     @type string $id             Field ID. Must be unique.
 	 *     @type string $name           Optional. Field name also used as label.
 	 *     @type string $type           Optional. Field type that determines which control is used.
 	 *     @type string $default        Optional. Default value used if field value is not set.
-	 *     @type string $value          Optional. Field value.
 	 *     @type string $tooltip        Optional. Tooltip that clarifies field purpose.
 	 *     @type string $description    Optional. Description that clarifies field usage.
 	 *     @type string $wrapper_class  Optional. CSS class assigned to the field wrapper.
@@ -37,35 +60,74 @@ class Base_Field {
 	 *     @type array  $options        Optional. Field options for select/radios/checkboxes.
 	 * }
 	 */
-	public function __construct( array $atts ) {
-		$this->atts = $atts;
+	public function __construct( $id, array $args ) {
+		$this->args = wp_parse_args( $args, $this->default_args() );
 		$this->set_value();
 	}
 
 	/**
-	 * Gets field attributes.
+	 * Retrieves the default field arguments.
 	 *
 	 * @since 0.1.0
 	 *
-	 * @return array Field attributes.
+	 * @return array Default field arguments.
 	 */
-	public function get_atts() {
-		return $this->atts;
+	protected function default_args() {
+		// Set default attributes applicable to all field types.
+		$defaults = array(
+			'name'          => '',
+			'type'          => 'text',
+			'default'       => '',
+			'value'         => '',
+			'tooltip'       => '',
+			'description'   => '',
+			'wrapper_class' => '',
+			'name_element'  => 'span',
+			'placeholder'   => '',
+			'options'       => array(),
+		);
+
+		// Use type to determine if label is appropriate for accessibility.
+		if ( isset( $this->args['type'] ) ) {
+			switch ( $this->args['type'] ) {
+				case 'input':
+				case 'search':
+				case 'select':
+					$defaults['name_element'] = 'label';
+					break;
+			}
+		}
+
+		return $defaults;
 	}
 
 	/**
-	 * Sets field attributes.
+	 * Gets field arguments.
 	 *
 	 * @since 0.1.0
 	 *
-	 * @param array $atts Associative array of field attributes.
+	 * @return array Field arguments.
 	 */
-	public function set_atts( $atts ) {
-		$this->atts = $atts;
+	public function get_args() {
+		return $this->args;
+	}
+
+	/**
+	 * Gets field value.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return mixed $value Field value.
+	 */
+	public function get_value() {
+		return $this->value();
 	}
 
 	/**
 	 * Sets field value.
+	 *
+	 * Field value is set to the passed argument. If an argument is not set,
+	 * then the field will attempt to set a default value.
 	 *
 	 * @since 0.1.0
 	 *
@@ -73,37 +135,11 @@ class Base_Field {
 	 */
 	public function set_value( $value = null ) {
 		if ( null === $value ) {
-			// No value exists, so set value to default.
-			$this->set_att( 'value', $this->atts['default'] );
+			// No value passed, so set value to default.
+			$this->value = isset( $this->args['default'] ) ? $this->args['default'] : '';
 		} else {
-			// Value exists, so set it accordingly.
-			$this->set_att( 'value', $value );
+			$this->value = $value;
 		}
-	}
-
-	/**
-	 * Gets a single field attribute.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $att_key Key associated with the field attribute.
-	 *
-	 * @return string|array Value of the field attribute if it exists.
-	 */
-	public function get_att( $att_key ) {
-		return isset( $this->atts[ $att_key ] )	? $this->atts[ $att_key ] : '';
-	}
-
-	/**
-	 * Gets a single field attribute.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $key   Key associated with the field attribute.
-	 * @param string $value Value of the field attribute.
-	 */
-	public function set_att( $key, $value ) {
-		$this->atts[ $key ] = $value;
 	}
 
 	/**
@@ -115,11 +151,10 @@ class Base_Field {
 	 *                             or an instance of a View object.
 	 * @param array|null  $context Optional. Context variables for use in view.
 	 */
-	public function render( $view ) {
+	public function render( $view, $context ) {
 		$view_object = is_string( $view ) ? new View( $view ) : $view;
 		$view_object->render(
-			$this->get_atts(),
-			true
+			$context
 		);
 	}
 }
--- includes/field/class-field-factory.php
+++ includes/field/class-field-factory.php
@@ -1,72 +1,35 @@
 <?php
-
 /**
  * Defines the Field_Factory class
  *
- * @package WP_Business_Reviews\Includes\Fields_API
+ * @package WP_Business_Reviews\Includes\Field
  * @since   0.1.0
  */
 
 namespace WP_Business_Reviews\Includes\Field;
 
 /**
- * Creates a Field object based on provided attributes.
+ * Creates a Field object based on provided arguments.
  *
  * @since 0.1.0
- * @see   Request
+ *
+ * @see Field
  */
 class Field_Factory {
-	/**
-	 * Field attributes.
-	 *
-	 * @since 0.1.0
-	 * @access private
-	 * @var array
-	 */
-	protected $atts;
-
 	/**
 	 * Creates a new instance of a Field object.
 	 *
 	 * @since 0.1.0
-	 * @see Field
 	 *
-	 * @param array $atts Field attributes.
+	 * @see WP_Business_Reviews\Includes\Field\Base_Field
+	 *
+	 * @param string $id   Unique identifier of the field.
+	 * @param array  $args Field arguments.
 	 *
 	 * @return Field|boolean Instance of Field class or false.
 	 */
-	public static function create( array $atts = array() ) {
-		if ( ! isset( $atts['id'] ) ) {
-			return false;
-		}
-
-		// Set default attributes applicable to all field types.
-		$defaults = array(
-			'id'            => '',
-			'name'          => '',
-			'type'          => 'text',
-			'default'       => '',
-			'value'         => '',
-			'tooltip'       => '',
-			'description'   => '',
-			'wrapper_class' => '',
-			'name_element'  => 'span',
-			'placeholder'   => '',
-			'options'       => array(),
-		);
-
-		// Use label element for certain field types.
-		switch ( $atts['type'] ) {
-			case 'input':
-			case 'search':
-			case 'select':
-				$defaults['name_element'] = 'label';
-				break;
-		}
-
-		// Merge provided field attributes with default attributes.
-		$atts = wp_parse_args( $atts, $defaults );
-
-		return new Base_Field( $atts );
+	public static function create( $id, array $args = array() ) {
+		// TODO: Add logic to return different field subclasses based on type.
+		return new Base_Field( $id, $args );
 	}
 }
