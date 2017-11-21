10b41e500d6db55611d001f5b4c470dc48ffa315
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-20 19:01:08 -0500

    Format new Settings_API class

--- includes/settings/class-settings-api.php
+++ includes/settings/class-settings-api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Defines the Settings class
+ * Defines the Settings_API class
  *
  * @link https://wpbusinessreviews.com
  *
@@ -10,14 +10,24 @@
 
 namespace WP_Business_Reviews\Includes\Settings;
 
+use WP_Business_Reviews\Includes\Config;
+
 /**
- * Handles the custom settings for the plugin.
+ * Handles the custom Settings API for the plugin.
  *
  * This plugin does not use the WordPress Settings API.
  *
  * @since 0.1.0
  */
-class Settings {
+class Settings_API {
+	/**
+	 * Settings config.
+	 *
+	 * @since 0.1.0
+	 * @var Config $config
+	 */
+	protected $config;
+
 	/**
 	 * Name of the option that stores plugin settings.
 	 *
@@ -43,7 +53,7 @@ class Settings {
 	protected $fields;
 
 	/**
-	 * Instantiates the Settings object.
+	 * Instantiates the Settings_API object.
 	 *
 	 * The required Config object must contain field definitions that can be
 	 * parsed into settings.
@@ -52,9 +62,18 @@ class Settings {
 	 *
 	 * @see WP_Business_Reviews\Includes\Config
 	 *
-	 * @param Config Settings config.
+	 * @param string|Config $config Settings config.
 	 */
-	public function __construct( Config $config ) {
-		$this->config = $config;
+	public function __construct( $config, $option ) {
+		$this->config = is_string( $config ) ? new Config( $config ) : $config;
+	}
+
+	/**
+	 * Initializes the object for use.
+	 *
+	 * @since 0.1.0
+	 */
+	public function init() {
+		$this->settings = get_option( $this->option, array() );
 	}
 }
