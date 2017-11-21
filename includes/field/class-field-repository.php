99b03a6c69b1063d7f7a0b3f30c44eaa6235eebc
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-21 14:18:33 -0500

    Extend Repository to Field_Repository

--- includes/field/class-field-repository.php
+++ includes/field/class-field-repository.php
@@ -11,31 +11,10 @@
 namespace WP_Business_Reviews\Includes\Field;
 
 /**
- * Stores Field objects.
- *
- * The field repository provides persistent storage for Field objects used
- * throughout the plugin. By housing Field objects in a single location, the
- * fields can be referenced for multiple purposes.
+ * Holds Field objects.
  *
  * @since 0.1.0
  */
-class Field_Repository {
-	/**
-	 * Stored instances.
-	 *
-	 * @since 0.1.0
-	 * @var array $items
-	 */
-	private $items;
+class Field_Repository extends Repository {
 
-	/**
-	 * Instantiates the Field_Repository object.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param Field[] $items Array of Field objects.
-	 */
-	public function __construct( array $items = array() ) {
-		$this->items = $items;
-	}
 }
