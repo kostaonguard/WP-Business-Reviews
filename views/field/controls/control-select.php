95dbafbf5edaaf7ca657a41b2223a41cdae5b365
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-20 22:10:30 -0500

    Remove additional control attributes from select

--- views/field/controls/control-select.php
+++ views/field/controls/control-select.php
@@ -2,14 +2,6 @@
 	id="wpbr-control-<?php echo esc_attr( $this->id ); ?>"
 	class="wpbr-field__select js-wpbr-control"
 	name="<?php echo esc_attr( $this->id ); ?>"
-	<?php
-	// Output any additional control attributes.
-	if ( ! empty( $this->control_atts ) ) {
-		foreach ( $this->control_atts as $name => $value ) {
-			echo ' ' . esc_attr( $name ) . '="' . esc_attr( $value ) . '"';
-		}
-	}
-	?>
 	>
 	<?php foreach ( $this->options as $option_value => $option_text ) : ?>
 		<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $option_value === $this->value ); ?>>
