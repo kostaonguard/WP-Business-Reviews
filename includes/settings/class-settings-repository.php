8dcfddbb7d53c6acde242e79b0001241e6a40c2f
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-21 17:46:39 -0500

    Stub Settings_Repository

---
+++ includes/settings/class-settings-repository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Defines the Settings_Repository class
+ *
+ * @link https://wpbusinessreviews.com
+ *
+ * @package WP_Business_Reviews\Includes\Settings
+ * @since 0.1.0
+ */
+
+namespace WP_Business_Reviews\Includes\Settings;
+
+/**
+ * Holds settings.
+ *
+ * @since 0.1.0
+ */
+class Settings_Repository extends Repository {
+
+}
