88928341a6fe802d0d94d4d6056b5681eb88fdde
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-21 20:19:24 -0500

    Fix type errors when parsing the config

--- includes/field/class-field-config-parser.php
+++ includes/field/class-field-config-parser.php
@@ -10,6 +10,8 @@
 
 namespace WP_Business_Reviews\Includes\Field;
 
+use WP_Business_Reviews\Includes\Config;
+
 /**
  * Recursively parses Field objects from a Config.
  *
@@ -21,21 +23,24 @@ namespace WP_Business_Reviews\Includes\Field;
  */
 class Field_Config_Parser {
 	/**
-	 * Instantiates the Field_Config_Parser object.
+	 * Parses the Config as an array.
 	 *
 	 * @since 0.1.0
+	 *
 	 * @param string|Config $config Path to config or Config object.
+	 * @return Fields[] Associative array of Field objects.
 	 */
-	public function __construct( $config ) {
-		$this->config = is_string( $config ) ? new Config( $config ) : $config;
+	public function parse_config( $config ) {
+		$config = is_string( $config ) ? new Config( $config ) : $config;
+		return $this->parse_array( $config->getArrayCopy() );
 	}
 
 	/**
-	 * Recursively parses Field objects from a Config.
+	 * Recursively parses Field objects from an array.
 	 *
 	 * When the parser finds a 'fields' key, then each item within that array
-	 * is considered to be a complete field definition. The arguments within
-	 * the definition are used to create a new Field object.
+	 * is assumed to be a complete field definition. The arguments within the
+	 * definition are used to create a new Field object.
 	 *
 	 * @since 0.1.0
 	 *
@@ -43,17 +48,18 @@ class Field_Config_Parser {
 	 *
 	 * @return Fields[] Array of Field objects.
 	 */
-	public function parse( array $array ) {
+	protected function parse_array( array $array ) {
 		$field_objects = array();
 
-		foreach ( $config as $key => $value ) {
-			if ( isset( $key['fields'] ) ) {
-				foreach( $key['fields'] as 'field_id' => 'field_args' ) {
+		foreach ( $array as $key => $value ) {
+			if ( isset( $value['fields'] ) ) {
+				foreach ( $value['fields'] as $field_id => $field_args ) {
 					$field_objects[] = Field_Factory::create( $field_id, $field_args );
 				}
 			} else {
+				echo 'no';
 				if ( is_array( $value ) ) {
-					$this->parse( $value );
+					$this->parse_array( $value );
 				}
 			}
 		}
