dd50bece4e54d3b3d7914fb94cc5ec1e138bec46
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-22 19:07:47 -0500

    Remove debug echo

--- views/field/field-main.php
+++ views/field/field-main.php
@@ -4,7 +4,6 @@
 	>
 	<?php
 	if ( ! empty( $this->args['name'] ) ) {
-		echo $this->args['name_element'];
 		if ( ! empty( $this->args['name_element'] ) && 'label' === $this->args['name_element'] ) {
 			// Render field name in label tag.
 			$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-label.php' );
