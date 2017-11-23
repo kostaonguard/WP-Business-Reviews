95a4432fcca6e5a300ff3baed65f469e99a09f70
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-23 03:38:41 -0500

    Attempt to get option from database if not in repository

--- includes/settings/class-option-repository.php
+++ includes/settings/class-option-repository.php
@@ -18,5 +18,23 @@ use WP_Business_Reviews\Includes\Repository;
  * @since 0.1.0
  */
 class Option_Repository extends Repository {
-
+	/**
+	 * Gets an option by its primary key or identifier.
+	 *
+	 * If the option does not exist in the repository, it will be retrieved
+	 * from the WordPress database or object cache.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $option_name Name of the option to return.
+	 * @param mixed  $default     Optional. Default value to return if the option does not exist.
+	 * @return mixed A single value from the repository.
+	 */
+	public function get( $option_name, $default = false ) {
+		if ( $this->has( $option_name ) ) {
+			return $this->values[ $option_name ];
+		} else {
+			return get_option( $option_name, $default );
+		}
+	}
 }
