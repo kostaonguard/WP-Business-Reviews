fe940258e084acb7de5de99eff8ea667a5051a66
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-27 18:01:33 -0500

    Update foreach loop to read tab ID

--- views/settings/settings-tabs.php
+++ views/settings/settings-tabs.php
@@ -1,12 +1,12 @@
 <nav>
 	<ul class="wpbr-tabs js-wpbr-tabs">
-		<?php foreach ( $this->field_hierarchy as $tab ) : ?>
+		<?php foreach ( $this->config as $tab_id => $tab ) : ?>
 			<li class="wpbr-tabs__item">
 				<a
-					id="wpbr-tab-<?php echo esc_attr( $tab['id'] ); ?>"
+					id="wpbr-tab-<?php echo esc_attr( $tab_id ); ?>"
 					class="wpbr-tabs__link js-wpbr-tab"
-					href="#wpbr-panel-<?php echo esc_attr( $tab['id'] ); ?>"
-					data-tab-id="<?php echo esc_attr( $tab['id'] ); ?>
+					href="#wpbr-panel-<?php echo esc_attr( $tab_id ); ?>"
+					data-tab-id="<?php echo esc_attr( $tab_id ); ?>
 					">
 					<?php echo esc_html( $tab['name'] ); ?>
 				</a>
