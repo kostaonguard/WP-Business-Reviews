5cc9d4c23418f8a46109f5d89724f4b9adfde92a
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-27 18:03:32 -0500

    Render field from repository

--- views/settings/panel/panel-main.php
+++ views/settings/panel/panel-main.php
@@ -1,7 +1,6 @@
 <div class="wpbr-panel__main">
-	<?php foreach ( $this->sections as $section ) : ?>
-		<?php $save_button = isset( $section['save_button'] ) ? $section['save_button'] : true; ?>
-		<div id="wpbr-section-<?php echo esc_attr( $section['id'] ); ?>" class="wpbr-panel__section js-wpbr-section" data-subtab-id="<?php echo esc_attr( $section['id'] ); ?>">
+	<?php foreach ( $this->sections as $section_id => $section ) : ?>
+		<div id="wpbr-section-<?php echo esc_attr( $section_id ); ?>" class="wpbr-panel__section js-wpbr-section" data-subtab-id="<?php echo esc_attr( $section_id ); ?>">
 			<div class="wpbr-admin-header">
 				<h2 class="wpbr-admin-header__heading"><?php echo esc_html( $section['heading'] ); ?></h2>
 
@@ -20,17 +19,17 @@
 				 *
 				 * @since 0.1.0
 				 */
-				do_action( 'wpbr_settings_notices_' . $section['id'] );
+				do_action( 'wpbr_settings_notices_' . $section_id );
 				?>
 			</div>
 
 			<?php if ( ! empty( $section['fields'] ) ) : ?>
 				<form method="post">
-					<input type="hidden" name="wpbr_section" value="<?php echo esc_attr( $section['id'] ); ?>">
+					<input type="hidden" name="wpbr_section" value="<?php echo esc_attr( $section_id ); ?>">
 					<?php
-					// Render settings fields.
-					foreach ( $section['fields'] as $field ) {
-						$field->render( WPBR_PLUGIN_DIR . 'views/field/field-main.php' );
+					foreach ( $section['fields'] as $field_id => $field_args ) {
+						// Render the field object that matches the field ID present in the config.
+						$field_object = $this->field_repository->get( $field_id )->render();
 					}
 					?>
 				</form>
