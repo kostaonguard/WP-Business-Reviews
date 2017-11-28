b2acb1a7f108987e3bcc602c899ec02a86ad9cd8
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-28 00:41:01 -0500

    Pass tab ID as context to panel view

--- views/settings/settings-panels.php
+++ views/settings/settings-panels.php
@@ -1,11 +1,11 @@
-<?php foreach ( $this->config as $panel_id => $panel ) : ?>
-	<div id="wpbr-panel-<?php echo esc_attr( $panel_id ); ?>" class="wpbr-panel js-wpbr-panel" data-panel-id="<?php echo esc_attr( $panel_id ); ?>">
+<?php foreach ( $this->config as $tab_id => $tab ) : ?>
+	<div id="wpbr-panel-<?php echo esc_attr( $tab_id ); ?>" class="wpbr-panel js-wpbr-panel" data-tab-id="<?php echo esc_attr( $tab_id ); ?>">
 		<?php
 		// Render the panel navigation if more than one section exists.
-		if ( ! empty( $panel['sections'] ) && count( $panel['sections'] ) > 1 ) {
+		if ( ! empty( $tab['sections'] ) && count( $tab['sections'] ) > 1 ) {
 			$this->render_partial( WPBR_PLUGIN_DIR . 'views/settings/panel/panel-sidebar.php',
 				array(
-					'sections' => $panel['sections'],
+					'sections' => $tab['sections'],
 				)
 			);
 		}
@@ -14,7 +14,8 @@
 		$this->render_partial(
 			WPBR_PLUGIN_DIR . 'views/settings/panel/panel-main.php',
 			array(
-				'sections' => $panel['sections'],
+				'tab_id'   => $tab_id,
+				'sections' => $tab['sections'],
 				'field_repository' => $this->field_repository,
 			)
 		);
