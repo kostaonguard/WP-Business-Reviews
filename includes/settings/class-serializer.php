9c0f1f49c690e2c5611f937f41a20d3075c39212
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-28 01:37:15 -0500

    Improve redirect method to reflect active tab

--- includes/settings/class-serializer.php
+++ includes/settings/class-serializer.php
@@ -31,7 +31,7 @@ class Serializer {
 	 * @since 0.1.0
 	 */
 	public function save() {
-		error_log( '<pre>' . print_r( $_POST, true ) . '</pre>' );
+		// error_log( print_r( $_POST, true ) );
 		// TODO: First, validate the nonce.
         // TODO: Secondly, verify the user has permission to save.
 		// If the above are valid, save the option.
@@ -70,15 +70,44 @@ class Serializer {
 	/**
 	 * Redirect to the page from which settings were saved.
 	 *
+	 * If an active tab or subtab is provided, it will be included in the redirect URL.
+	 *
 	 * @since 0.1.0
 	 */
 	private function redirect() {
-		// TODO: Redirect back to active tab
-		if ( ! isset( $_POST['_wp_http_referer'] ) ) {
-			$_POST['_wp_http_referer'] = wp_login_url();
+		$active_tab = $active_subtab = $referer = '';
+
+		if ( ! empty( $_POST['wp_business_reviews_tab'] ) ) {
+			$active_tab = sanitize_text_field( wp_unslash( $_POST['wp_business_reviews_tab'] ) );
 		}
-		$url = sanitize_text_field( wp_unslash( $_POST['_wp_http_referer'] ) );
-		wp_safe_redirect( urldecode( $url ) );
+
+		if ( ! empty( $_POST['wp_business_reviews_subtab'] ) ) {
+			$active_subtab = sanitize_text_field( wp_unslash( $_POST['wp_business_reviews_subtab'] ) );
+		}
+
+		if ( ! empty( $_POST['_wp_http_referer'] ) ) {
+			$referer = sanitize_text_field( wp_unslash( $_POST['_wp_http_referer'] ) );
+		} else {
+			$referer = wp_login_url();
+		}
+
+		// Parse referer into path and query.
+		$parsed_url = parse_url( $referer );
+
+		// Parse query string into array of query parts.
+		parse_str( $parsed_url['query'], $query_parts );
+
+		// Update active tab and subtab.
+		$query_parts['wpbr_tab'] = $active_tab;
+		$query_parts['wpbr_subtab'] = $active_subtab;
+
+		// Stringify the query string parts.
+		$query_string = http_build_query( $query_parts );
+
+		// Assemble the redirect location.
+		$redirect = $parsed_url['path'] . '?' . $query_string;
+
+		wp_safe_redirect( $redirect );
 		exit;
 	}
 }
