bfba139cbd0417a37d512012856a37fe2f34643f
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-27 21:41:30 -0500

    Update nonce to use wp_business_reviews_

--- views/field/controls/control-save.php
+++ views/field/controls/control-save.php
@@ -1,9 +1,7 @@
 <?php
-wp_nonce_field( 'wpbr_settings_save', 'wpbr_settings_nonce' );
-submit_button(
-	__( 'Save Changes', 'wp-business-reviews' ),
-	'primary',
-	$this->id,
-	false
+wp_nonce_field(
+	'wp_business_reviews_save_settings',
+	'wp_business_reviews_settings_nonce'
 );
+submit_button();
 ?>
