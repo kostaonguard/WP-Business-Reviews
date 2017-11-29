fd881ee98f6307abb7ab4c02b11838672e67e267
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-29 06:17:01 -0500

    Add Deserializer class to retrieve info from db

---
+++ includes/settings/class-deserializer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Defines the Deserializer class
+ *
+ * @link https://wpbusinessreviews.com
+ *
+ * @package WP_Business_Reviews\Includes\Settings
+ * @since 0.1.0
+ */
+
+namespace WP_Business_Reviews\Includes\Settings;
+
+/**
+ * Retrieves information from the database.
+ *
+ * @since 0.1.0
+ */
+class Deserializer {
+	/**
+	 * Retrieves value that matches the specified key.
+	 *
+	 * @since 0.1.0
+	 */
+	public function get_value( $key, $default ) {
+		return get_option( 'wp_business_reviews_' . $key, $default );
+	}
+}
