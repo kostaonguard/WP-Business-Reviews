4d95efe6412d408117402357bffe7091c6c98c2d
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-29 14:20:38 -0500

    Add condition to check value of platform status

--- views/field/controls/control-platform-status.php
+++ views/field/controls/control-platform-status.php
@@ -1,1 +1,9 @@
-<strong class="wpbr-platform-status"><?php echo esc_html( 'Not Connected' ); ?></strong>
+<?php if ( 'connected' === $this->value ) : ?>
+	<strong class="wpbr-platform-status wpbr-platform-status--success">
+		<?php echo esc_html__( 'Connected', 'wp-business-reviews' ); ?>
+	</strong>
+<?php else : ?>
+	<strong class="wpbr-platform-status wpbr-platform-status--error">
+		<?php echo esc_html__( 'Not Connected', 'wp-business-reviews' ); ?>
+	</strong>
+<?php endif; ?>
