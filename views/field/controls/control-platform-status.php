00e9316c7516ca4cd6617b7bd9b9558c01e5a9b2
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-11-29 14:22:52 -0500

    Add input to store value of platform status

--- views/field/controls/control-platform-status.php
+++ views/field/controls/control-platform-status.php
@@ -7,3 +7,11 @@
 		<?php echo esc_html__( 'Not Connected', 'wp-business-reviews' ); ?>
 	</strong>
 <?php endif; ?>
+
+<input
+	id="wpbr-control-<?php echo esc_attr( $this->id ); ?>"
+	class="wpbr-field__input"
+	type="text"
+	name="wp_business_reviews_settings[<?php echo esc_attr( $this->id ); ?>]"
+	value="<?php echo esc_attr( $this->value ); ?>"
+	>
