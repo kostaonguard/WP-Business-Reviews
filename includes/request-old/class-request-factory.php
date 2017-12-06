7e17696f5e1edeb8d5f605b69c9a5b17cb755ae3
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-06 00:08:13 -0500

    Move old request classes into separate directory

---
+++ includes/request-old/class-request-factory.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Defines the Request_Factory class
+ *
+ * @package WP_Business_Reviews\Includes\Request
+ * @since   0.1.0
+ */
+
+namespace WP_Business_Reviews\Includes\Request;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Determines the appropriate Request subclass based on platform.
+ *
+ * @since 0.1.0
+ * @see   Request
+ */
+class Request_Factory {
+
+	/**
+	 * Creates a new instance of the Request subclass.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $business_id ID of the business on the platform.
+	 * @param string $platform    Reviews platform used in the request.
+	 *
+	 * @return Request Instance of Request for the provided platform.
+	 */
+	public static function create( $business_id, $platform ) {
+		switch ( $platform ) {
+			case 'google_places' :
+				return new Google_Places_Request( $business_id, $platform );
+			case 'facebook' :
+				return new Facebook_Request( $business_id, $platform );
+			case 'yelp' :
+				return new Yelp_Request( $business_id, $platform );
+			case 'yp' :
+				return new YP_Request( $business_id, $platform );
+			case 'wp_org' :
+				return new WP_Org_Request( $business_id, $platform );
+		}
+
+	}
+
+}
