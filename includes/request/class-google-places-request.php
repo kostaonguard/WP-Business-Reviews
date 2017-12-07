22bc5d720a994a3007ac90255779f69724fee61c
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-07 17:46:11 -0500

    Use key property instead of get_key() method

--- includes/request/class-google-places-request.php
+++ includes/request/class-google-places-request.php
@@ -58,7 +58,7 @@ class Google_Places_Request extends Request_Base {
 		$url = add_query_arg(
 			array(
 				'query' => $term . ', ' . $location,
-				'key'   => $this->get_key(),
+				'key'   => $this->key,
 			),
 			'https://maps.googleapis.com/maps/api/place/textsearch/json'
 		);
@@ -79,7 +79,7 @@ class Google_Places_Request extends Request_Base {
 		$url = add_query_arg(
 			array(
 				'place_id' => $id,
-				'key'      => $this->get_key(),
+				'key'      => $this->key,
 			),
 			'https://maps.googleapis.com/maps/api/place/details/json'
 		);
