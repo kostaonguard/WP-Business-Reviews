bf9e969ea762f31cc64483e0f50f067289d11358
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-07 05:30:17 -0500

    Add post and process_response methods

--- includes/request/class-request-base.php
+++ includes/request/class-request-base.php
@@ -17,9 +17,6 @@ class Request_Base {
 	/**
 	 * Retrieves a response from a safe HTTP request using the GET method.
 	 *
-	 * This method is a wrapper for `wp_safe_remote_get()` which means the URL
-	 * is validated to avoid redirection and request forgery attacks.
-	 *
 	 * @since 0.1.0
 	 *
 	 * @see wp_safe_remote_get()
@@ -29,6 +26,31 @@ class Request_Base {
 	public function get( $url, $args = array() ) {
 		$response = wp_safe_remote_get( $url, $args );
 
+		return $this->process_response( $response );
+	}
+
+	/**
+	 * Retrieves a response from a safe HTTP request using the POST method.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @see wp_safe_remote_post()
+	 *
+	 * @return array Associative array containing the response body.
+	 */
+	public function post( $url, $args = array() ) {
+		$response = wp_safe_remote_post( $url, $args );
+
+		return $this->process_response( $response );
+	}
+
+	/**
+	 * Validates and decodes the response body.
+	 *
+	 * @param mixed $response The raw response.
+	 * @return mixed Associative array of the response body.
+	 */
+	private function process_response( $response ) {
 		if ( is_wp_error( $response ) ) {
 			// TODO: Return WP_Error here for failed responses.
 			return false;
