a16957a3491d3b573e60df5b0886f07097ea6bfc
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-07 23:09:48 -0500

    Use setting value for status and last checked.

--- views/field/controls/control-platform-status.php
+++ views/field/controls/control-platform-status.php
@@ -1,5 +1,5 @@
 <div class="wpbr-field__flex">
-	<?php if ( 'connected' === $this->value ) : ?>
+	<?php if ( 'connected' === $this->value['status'] ) : ?>
 		<strong class="wpbr-platform-status wpbr-platform-status--success">
 			<?php echo esc_html__( 'Connected', 'wp-business-reviews' ); ?>
 		</strong>
@@ -12,12 +12,13 @@
 	<button class="button wpbr-field__inline-button js-wpbr-refresh-button"><?php esc_html_e( 'Refresh', 'wp-business-reviews' ); ?></button>
 </div>
 
-<p id="wpbr-field-description-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__description">
-	<?php
+<?php if ( isset( $this->value['last_checked'] ) ) : ?>
+	<p id="wpbr-field-description-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__description">
+		<?php
 		$now          = time();
-		// TODO: Get last checked time from field value.
-		$last_checked = 1511568000;
+		$last_checked = $this->value['last_checked'];
 		$difference   = human_time_diff( $last_checked, $now );
 		printf( esc_html__( 'Last checked %s ago.', 'wp-business-reviews' ), $difference );
-	?>
-</p>
+		?>
+	</p>
+<?php endif; ?>
