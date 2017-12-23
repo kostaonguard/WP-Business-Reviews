1fe535ce8015b0b71a108ffbe84ffebb68be5a0e
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-23 16:17:28 -0500

    Add platform query args to each platform link

--- views/builder/builder-launcher.php
+++ views/builder/builder-launcher.php
@@ -4,8 +4,11 @@
 			<div class="wpbr-card">
 				<h2><?php esc_html_e( 'Select a platform to build a new Review Set...', 'wp-business-reviews' ); ?></h2>
 				<ul>
-					<?php foreach ( $this->active_platforms as $platform_id => $platform_name ) : ?>
-						<li><?php esc_html_e( $platform_name ); ?></li>
+					<?php
+					foreach ( $this->active_platforms as $platform_id => $platform_name ) :
+						$platform_url = add_query_arg( array( 'wpbr_platform' => $platform_id ) );
+					?>
+						<li><a href="<?php echo esc_url( $platform_url ); ?>"><?php esc_html_e( $platform_name ); ?></a></li>
 					<?php endforeach; ?>
 				</ul>
 			</div>
