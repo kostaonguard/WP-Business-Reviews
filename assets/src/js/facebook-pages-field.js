5f82914b104c1e79cef0acec27ba35a6957d70c6
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-29 01:08:57 -0500

    Stub FacebookPagesField JS class

---
+++ assets/src/js/facebook-pages-field.js
@@ -0,0 +1,19 @@
+import Field from './field';
+
+class FacebookPagesField extends Field {
+	constructor( name ) {
+		super( name );
+	}
+
+	disconnect() {
+
+		// Disconnect Facebook.
+	}
+
+	registerEventHandlers() {
+
+		// Add 'click' event listener to Disconnect button.
+	}
+}
+
+export default FacebookPagesField;
