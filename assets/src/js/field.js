cbc8ad35db81f8cb28b04846c11237c1e8c47ab3
Kevin Hoffman <kevin@kevinwhoffman.com>
2017-12-29 01:04:58 -0500

    Simplify JS Field class

--- assets/src/js/field.js
+++ assets/src/js/field.js
@@ -1,16 +1,11 @@
 class Field {
 	constructor( name ) {
-		this.field = document.getElementById( `wpbr-field-${name}` );
-		this.label = document.getElementById( `wpbr-label-${name}` );
-		this.tooltip = document.getElementById( `wpbr-tooltip-${name}` );
 		this.control = document.getElementById( `wpbr-control-${name}` );
-		this.description = document.getElementById( `wpbr-description-${name}` );
 	}
 
-	getValue() {
+	get value() {
 		return this.control.value;
 	}
-
 }
 
 export default Field;
