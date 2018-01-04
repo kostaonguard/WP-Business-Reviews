fc030ca5cf1a9f8dbd69f47d3a13fe34a168e33b
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-04 17:34:02 -0500

    Fix indentation of field main view

--- views/field/field-main.php
+++ views/field/field-main.php
@@ -20,20 +20,20 @@ $field_class_att = implode( $field_classes, ' ' );
 	data-wpbr-field-type="<?php echo esc_attr( $this->args['type'] ); ?>"
 >
 
-<?php
-if ( ! empty( $this->args['name'] ) ) {
-	if ( ! empty( $this->args['name_element'] ) && 'label' === $this->args['name_element'] ) {
-		// Render field name in label tag.
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-label.php' );
-	} else {
-		// Render field name in span tag (intended for use alongside fieldsets).
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-name.php' );
-	}
-}
-?>
-
-<div id="wpbr-field-control-wrap-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__control-wrap">
 	<?php
+	if ( ! empty( $this->args['name'] ) ) {
+		if ( ! empty( $this->args['name_element'] ) && 'label' === $this->args['name_element'] ) {
+			// Render field name in label tag.
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-label.php' );
+		} else {
+			// Render field name in span tag (intended for use alongside fieldsets).
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/field-name.php' );
+		}
+	}
+	?>
+
+	<div id="wpbr-field-control-wrap-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__control-wrap">
+		<?php
 		// Render field control.
 		$type = str_replace( '_', '-', $this->args['type'] );
 
