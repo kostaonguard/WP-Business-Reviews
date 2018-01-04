0342d93bd2252a1cf94dd0d3594f774445fc9449
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-04 13:17:58 -0500

    Improve clarity of field attributes

--- views/field/field-main.php
+++ views/field/field-main.php
@@ -1,6 +1,23 @@
+<?php
+$field_classes = array(
+	'wpbr-field',
+	"wpbr-field-{$this->args['type']}",
+	'js-wpbr-field',
+);
+
+// Add wrapper class if one is set.
+if ( ! empty( $this->args['wrapper_class'] ) ) {
+	$field_classes[] = $this->args['wrapper_class'];
+}
+
+// Convert classes from array to string.
+$field_class_att = implode( $field_classes, ' ' );
+?>
+
 <div
 id="wpbr-field-<?php echo esc_attr( $this->id ); ?>"
-class="wpbr-field wpbr-field--<?php echo esc_attr( $this->args['type'] ); ?> js-wpbr-field<?php echo ( $this->args['wrapper_class'] ? ' ' . esc_attr( $this->args['wrapper_class'] ) : '' ); ?>"
+class="<?php echo esc_attr( $field_class_att ); ?>"
+data-wpbr-field-type="<?php echo esc_attr( $this->args['type'] ); ?>"
 >
 
 <?php
