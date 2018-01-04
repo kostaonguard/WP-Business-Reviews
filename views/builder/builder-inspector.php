e5b940269c4408a432f7fd0586ab172cbb3b294d
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-04 13:25:54 -0500

    Fix section id typo

--- views/builder/builder-inspector.php
+++ views/builder/builder-inspector.php
@@ -1,6 +1,9 @@
 <div id="wpbr-builder-inspector" class="wpbr-builder__inspector">
 	<?php foreach ( $this->config as $section_id => $section ) : ?>
-		<div id="wpbr-section<?php echo esc_attr( $section_id ); ?>" class="wpbr-builder__section">
+		<div
+			id="wpbr-section-<?php echo esc_attr( $section_id ); ?>"
+			class="wpbr-builder__section"
+		>
 			<div class="wpbr-builder__section-header js-wpbr-section-header">
 				<button class="wpbr-builder__toggle"  aria-expanded="true">
 					<span class="screen-reader-text">Toggle section: <?php esc_html_e( $section['name'] ); ?></span>
