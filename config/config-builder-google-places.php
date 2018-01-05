26ec579860cf4af79073819cf327a4cf3487fe81
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-05 01:30:44 -0500

    Name config keys generically

--- config/config-builder-google-places.php
+++ config/config-builder-google-places.php
@@ -9,24 +9,25 @@
 namespace WP_Business_Reviews\Config;
 
 $config = array(
-	'google_places_search' => array(
+	'review_source' => array(
 		'name'   => __( 'Google Places Search', 'wp-business-reviews' ),
 		'fields' => array(
-			'google_places_search_terms' => array(
+			'search_terms' => array(
 				'name'        => __( 'Search Terms', 'wp-business-reviews' ),
 				'type'        => 'text',
 				'tooltip'     => __( 'Defines the terms used when searching for a business.', 'wp-business-reviews' ),
 				'placeholder' => __( 'Business Name or Type', 'wp-business-reviews' ),
 			),
-			'google_places_search_location' => array(
+			'search_location' => array(
 				'name'        => __( 'Location', 'wp-business-reviews' ),
 				'type'        => 'text',
 				'tooltip'     => __( 'Defines the location used when searching for a business.', 'wp-business-reviews' ),
 				'placeholder' => __( 'City, State, or Postal Code', 'wp-business-reviews' ),
 			),
-			'google_places_search_button' => array(
+			'search_button' => array(
 				'type'        => 'button',
 				'button_text' => __( 'Find Business', 'wp-business-reviews' ),
+				'value'       => 'search',
 			),
 		),
 	),
