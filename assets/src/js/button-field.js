e78ca6ae834d9fb271654695acb18e7594f9811b
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-05 01:32:35 -0500

    Add ButtonField JS class

---
+++ assets/src/js/button-field.js
@@ -0,0 +1,29 @@
+import Emitter from 'tiny-emitter';
+
+class ButtonField {
+	constructor( element ) {
+		this.root     = element;
+		this.control  = this.root.querySelector( '.js-wpbr-control' );
+		this.emitter  = new Emitter();
+	}
+
+	init() {
+		this.registerEventHandlers();
+	}
+
+	registerEventHandlers() {
+		this.control.addEventListener( 'click', event => {
+
+			// Get the control ID from the data attribute.
+			const controlId    = event.currentTarget.dataset.wpbrControlId;
+
+			// Get the control value.
+			const controlValue = event.currentTarget.value;
+
+			// Emit custom event that passes the control ID and value that changed.
+			this.emitter.emit( 'wpbrfieldchange', controlId, controlValue );
+		});
+	}
+}
+
+export default ButtonField;
