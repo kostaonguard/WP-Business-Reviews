3cdfffd505ec916f10f2f358109a8374261dbd25
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-05 01:29:53 -0500

    Prefix data attribute for control ID

--- views/field/controls/control-checkboxes.php
+++ views/field/controls/control-checkboxes.php
@@ -9,7 +9,7 @@
 					class="wpbr-field__checkbox js-wpbr-control"
 					name="wp_business_reviews_settings[<?php echo esc_attr( $this->id ); ?>][<?php echo esc_attr( $option_value ); ?>]"
 					value="<?php echo esc_attr( $option_value ); ?>"
-					data-control-type="<?php echo esc_attr( $option_value ); ?>"
+					data-wpbr-control-id="<?php echo esc_attr( $option_value ); ?>"
 					<?php checked( $this->value && in_array( $option_value, $this->value ) ); ?>
 					>
 				<label for="wpbr-control-<?php echo esc_attr( $option_value ); ?>">
--- views/field/controls/control-input.php
+++ views/field/controls/control-input.php
@@ -5,5 +5,5 @@
 	name="wp_business_reviews_settings[<?php echo esc_attr( $this->id ); ?>]"
 	value="<?php echo esc_attr( $this->value ); ?>"
 	placeholder="<?php echo esc_attr( $this->args['placeholder'] ); ?>"
-	data-control-type="<?php echo esc_attr( $this->id ); ?>"
+	data-wpbr-control-id="<?php echo esc_attr( $this->id ); ?>"
 	>
