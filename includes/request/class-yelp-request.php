711b80eb772d2763072816a101ff83d2a042ffa1
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-08 18:37:12 -0500

    Normalize Yelp review source searches

--- includes/request/class-yelp-request.php
+++ includes/request/class-yelp-request.php
@@ -45,18 +45,18 @@ class Yelp_Request extends Request {
 	}
 
 	/**
-	 * Retrieves search results based on a search terms and location.
+	 * Searches review sources based on search terms and location.
 	 *
 	 * @since 0.1.0
 	 *
 	 * @param string $terms    The search terms, usually a business name.
 	 * @param string $location The location within which to search.
-	 * @return array Associative array containing the response body.
+	 * @return array Array containing normalized review sources.
 	 */
-	public function search( $terms, $location ) {
+	public function search_review_source( $terms, $location ) {
 		$url = add_query_arg(
 			array(
-				'terms'    => $terms,
+				'term'     => $terms,
 				'location' => $location,
 			),
 			'https://api.yelp.com/v3/businesses/search'
@@ -71,7 +71,14 @@ class Yelp_Request extends Request {
 
 		$response = $this->get( $url, $args );
 
-		return $response;
+		if ( isset( $response['businesses'] ) ) {
+			foreach( $response['businesses'] as $review_source ) {
+				$review_sources[] = $this->normalize_review_source( $review_source );
+			}
+		} else {
+			return new \WP_Error( 'invalid_response_structure', __( 'Response could not be normalized due to invalid response structure.', 'wp-business-reviews' ) );
+		}
+		return $review_sources;
 	}
 
 	/**
@@ -98,6 +105,96 @@ class Yelp_Request extends Request {
 	}
 
 	/**
+	 * Normalizes and sanitize a raw review source from the platform API.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $raw_review_source Review source data from platform API.
+	 * @return array|WP_Error Standardized review source properties or WP_Error
+	 *                        if response structure is invalid.
+	 */
+	public function normalize_review_source( array $raw_review_source ) {
+		$r = $raw_review_source;
+		$r_clean = array();
+
+		// Set ID of the review source on the platform.
+		if ( isset( $r['id'] ) ) {
+			$r_clean['platform_id'] =  $this->clean( $r['id'] );
+		}
+
+		// Set name.
+		if ( isset( $r['name'] ) ) {
+			$r_clean['name'] =  $this->clean( $r['name'] );
+		}
+
+		// Set page URL.
+		if ( isset( $r['url'] ) ) {
+			$r_clean['url'] = $this->clean( $r['url'] );
+		}
+
+		// Set rating.
+		if ( isset( $r['rating'] ) ) {
+			$r_clean['rating'] = $this->clean( $r['rating'] );
+		}
+
+		// Set icon and image to the same value since Yelp only provides one image.
+		if ( isset( $r['image_url'] ) ) {
+			$r_clean['icon'] = $this->clean( $r['image_url'] );
+			$r_clean['image'] = $r_clean['icon'];
+		}
+
+		// Set phone.
+		if ( isset( $r['display_phone'] ) ) {
+			$r_clean['phone'] =  $this->clean( $r['display_phone'] );
+		}
+
+		// Set formatted address.
+		if ( isset( $r['location']['display_address'] ) ) {
+			$r_clean['formatted_address'] = $this->clean( $this->format_address( $r['location']['display_address'] ) );
+		}
+
+		// Set street address.
+		if ( isset( $r['location']['address1'] ) ) {
+			$r_clean['street_address'] = $this->clean( $r['location']['address1'] );
+		}
+
+		// Set city.
+		if ( isset( $r['location']['city'] ) ) {
+			$r_clean['city'] = $this->clean( $r['location']['city'] );
+		}
+
+		// Set state.
+		if ( isset( $r['location']['state'] ) ) {
+			$r_clean['state_province'] = $this->clean( $r['location']['state'] );
+		}
+
+		// Set postal code.
+		if ( isset( $r['location']['zip_code'] ) ) {
+			$r_clean['postal_code'] = $this->clean( $r['location']['zip_code'] );
+		}
+
+		// Set country.
+		if ( isset( $r['location']['country'] ) ) {
+			$r_clean['country'] = $this->clean( $r['location']['country'] );
+		}
+
+		// Set latitude.
+		if ( isset( $r['coordinates']['latitude']) ) {
+			$r_clean['latitude'] = $this->clean( $r['coordinates']['latitude'] );
+		}
+
+		// Set longitude.
+		if ( isset( $r['coordinates']['longitude']) ) {
+			$r_clean['longitude'] = $this->clean( $r['coordinates']['longitude'] );
+		}
+
+		// Merge clean response values with default values in case any values were not provided.
+		$review_source = $this->args = wp_parse_args( $r_clean, $this->get_review_source_defaults() );
+
+		return $review_source;
+	}
+
+	/**
 	 * Retrieves reviews based on Yelp business ID.
 	 *
 	 * @since 0.1.0
@@ -118,5 +215,9 @@ class Yelp_Request extends Request {
 		$response = $this->get( $url, $args );
 
 		return $response;
+	}
+
+	protected function format_address( $address_components ) {
+		return trim( implode( $address_components ), ',' );
 	}
 }
