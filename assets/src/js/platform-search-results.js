7116175b326eeb2a815c3f4112ac59321f483acb
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-08 17:42:36 -0500

    Use normalized review source props in results

--- assets/src/js/platform-search-results.js
+++ assets/src/js/platform-search-results.js
@@ -1,5 +1,6 @@
 class PlatformSearchResults {
-	constructor( platform ) {
+	constructor( element, platform ) {
+		this.root = element;
 		this.platform = platform;
 
 		// Set this.buttons.
@@ -21,42 +22,42 @@ class PlatformSearchResults {
 	populateResults( results ) {
 
 		// Create scrollable container to hold results.
-		this.root = document.createElement( 'div' );
-		this.root.className = 'wpbr-scrollable wpbr-scrollable--border';
+		this.scrollable = document.createElement( 'div' );
+		this.scrollable.className = 'wpbr-scrollable wpbr-scrollable--border';
 
 		// Create empty results list.
 		this.resultsList = document.createElement( 'ul' );
 		this.resultsList.className = 'wpbr-stacked-list wpbr-stacked-list--striped';
 
-		// Append empty list to results container.
-		this.root.appendChild( this.resultsList );
+		// Append empty results list to scrollable container.
+		this.scrollable.appendChild( this.resultsList );
 
-		// Append results to bottom of container.
-		this.termsField.root.parentNode.appendChild( this.root );
-
-		// TODO: Sanitize places markup before displaying.
+		// TODO: Translate strings in search results.
 		const listItemsMarkup = `
-			${results.map( place => `
+			${results.map( reviewSource => `
 				<li class="wpbr-stacked-list__item wpbr-stacked-list__item--border-bottom">
 					<div class="wpbr-media">
-						<div class="wpbr-media__figure wpbr-media__figure--icon">
-							<img src="${place.icon}"><br>
+						<div class="wpbr-media__figure wpbr-media__figure--icon js-wpbr-icon">
+							<img src="${reviewSource.icon}"><br>
 						</div>
 						<div class="wpbr-media__body">
 							<div class="wpbr-business">
-								<a class="wpbr-business__name" href="https://www.google.com/maps/place/?q=place_id:${place.place_id}" target="_blank" rel="noopener noreferrer">${place.name}</a><br>
-								${isNaN( place.rating ) ? '' : `<span class="wpbr-business__rating">${parseFloat( place.rating ).toFixed( 1 )} <span class="wpbr-stars wpbr-stars--google">★★★★★</span></span><br>` }
-								<span class="wpbr-business__address">${place.formatted_address}</span><br>
+								<a class="wpbr-business__name" href="${reviewSource.url}" target="_blank" rel="noopener noreferrer">${reviewSource.name}</a><br>
+								<span class="wpbr-business__rating">${parseFloat( reviewSource.rating ).toFixed( 1 )} <span class="wpbr-stars wpbr-stars--google">★★★★★</span></span>
+								<span class="wpbr-business__address">${reviewSource.formatted_address}</span><br>
 								<button class="button button-primary js-wpbr-get-reviews-button">Get Reviews</button>
 							</div>
 						</div>
 					</div>
 				</li>
-			` ).join( '' )}
+			` ).join( '' ) }
 		`;
 
 		// Update and display results list.
 		this.resultsList.innerHTML = listItemsMarkup;
+
+		// Append scrollable to bottom of root.
+		this.root.appendChild( this.scrollable );
 	}
 }
 
