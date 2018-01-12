817ff71b6f63561497fe7ee7b32a2694bf309f17
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-12 00:00:17 -0500

    Remove control-wrap from platform-search view

--- views/field/partials/types/platform-search.php
+++ views/field/partials/types/platform-search.php
@@ -2,19 +2,11 @@
 if ( ! empty( $this->args['name'] ) ) {
 	$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/partials/label.php' );
 }
-?>
 
-<div id="wpbr-field-control-wrap-<?php echo esc_attr( $this->id ); ?>" class="wpbr-field__control-wrap">
+foreach ( $this->args['subfields'] as $subfield ) {
+	$subfield->render();
+}
 
-	<?php
-	foreach ( $this->args['subfields'] as $subfield ) {
-		$subfield->render();
-	}
-	?>
-
-	<?php
-	if ( ! empty( $this->args['description'] ) ) {
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/partials/description.php' );
-	}
-	?>
-</div>
+if ( ! empty( $this->args['description'] ) ) {
+	$this->render_partial( WPBR_PLUGIN_DIR . 'views/field/partials/description.php' );
+}
