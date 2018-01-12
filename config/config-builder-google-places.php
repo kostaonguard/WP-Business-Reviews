bb77b07f6f2cf79d994414ad22fd87161b4f6970
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-12 03:45:00 -0500

    Prefix platform_search fields

--- config/config-builder-google-places.php
+++ config/config-builder-google-places.php
@@ -15,25 +15,25 @@ $config = array(
 			'platform_search' => array(
 				'type'  => 'platform_search',
 				'subfields' => array(
-					'search_platform' => array(
+					'platform_search_platform' => array(
 						'type'        => 'hidden',
 						'value'       => 'google_places',
 					),
-					'search_terms' => array(
+					'platform_search_terms' => array(
 						'name'        => __( 'Search Terms', 'wp-business-reviews' ),
 						'type'        => 'text',
 						'value'       => 'coffee',
 						'tooltip'     => __( 'Defines the terms used when searching the Google Places API.', 'wp-business-reviews' ),
 						'placeholder' => __( 'Business name or type', 'wp-business-reviews' ),
 					),
-					'search_location' => array(
+					'platform_search_location' => array(
 						'name'        => __( 'Location', 'wp-business-reviews' ),
 						'type'        => 'text',
 						'value'       => 'Pittsburgh, PA',
 						'tooltip'     => __( 'Defines the location used when searching the Google Places API.', 'wp-business-reviews' ),
 						'placeholder' => __( 'City, state, or postal code', 'wp-business-reviews' ),
 					),
-					'search_button' => array(
+					'platform_search_button' => array(
 						'type'        => 'button',
 						'button_text' => __( 'Search', 'wp-business-reviews' ),
 						'value'       => 'search',
--- config/config-builder-yelp.php
+++ config/config-builder-yelp.php
@@ -15,25 +15,25 @@ $config = array(
 			'platform_search' => array(
 				'type'  => 'platform_search',
 				'subfields' => array(
-					'search_platform' => array(
+					'platform_search_platform' => array(
 						'type'        => 'hidden',
 						'value'       => 'yelp',
 					),
-					'search_terms' => array(
+					'platform_search_terms' => array(
 						'name'        => __( 'Search Terms', 'wp-business-reviews' ),
 						'type'        => 'text',
 						'value'       => 'tacos',
 						'tooltip'     => __( 'Defines the terms used when searching the Yelp API.', 'wp-business-reviews' ),
 						'placeholder' => __( 'Business name or type', 'wp-business-reviews' ),
 					),
-					'search_location' => array(
+					'platform_search_location' => array(
 						'name'        => __( 'Location', 'wp-business-reviews' ),
 						'type'        => 'text',
 						'value'       => 'San Diego, CA',
 						'tooltip'     => __( 'Defines the location used when searching the Yelp API.', 'wp-business-reviews' ),
 						'placeholder' => __( 'City, state, or postal code', 'wp-business-reviews' ),
 					),
-					'search_button' => array(
+					'platform_search_button' => array(
 						'type'        => 'button',
 						'button_text' => __( 'Search', 'wp-business-reviews' ),
 						'value'       => 'search',
