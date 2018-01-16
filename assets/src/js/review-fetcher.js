c2e559b6aa97de7bdd709d83237510b9e2fbc104
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-16 11:09:59 -0500

    Use Get Reviews buttons to trigger Ajax request

--- assets/src/js/review-fetcher.js
+++ assets/src/js/review-fetcher.js
@@ -1,28 +1,54 @@
 import Emitter from 'tiny-emitter';
+import axios from 'axios';
+import queryString from 'query-string';
 
 class ReviewFetcher {
 	constructor( element ) {
 		this.root    = element;
 		this.emitter = new Emitter();
-
-		console.log( 'ReviewFetcher constructor ran' );
-		console.log( this );
+		this.controls = this.root.querySelectorAll( '.js-wpbr-review-fetcher-button' );
 	}
 
 	init() {
-		this.controls = this.root.querySelectorAll( '.js-wpbr-review-fetcher-button' );
-		this.registerButtonEventHandlers();
+		this.registerControlEventHandlers();
 	}
 
-	registerButtonEventHandlers() {
+	registerControlEventHandlers() {
 		this.controls.forEach( ( control ) => {
 			control.addEventListener( 'click', ( event ) => {
-				const platform = event.currentTarget.getAttribute( 'data-wpbr-platform' );
-				const platformId = event.currentTarget.getAttribute( 'data-wpbr-platform-id' );
-				console.log( '==================================================' );
-				console.log( 'Requested: ' + platform, platformId );
+				const platform       = event.currentTarget.getAttribute( 'data-wpbr-platform' );
+				const reviewSourceId = event.currentTarget.getAttribute( 'data-wpbr-review-source-id' );
+
+				this.fetch( platform, reviewSourceId );
 			});
 		});
+	}
+
+	fetch( platform, reviewSourceId ) {
+		const response = axios.post(
+			ajaxurl,
+			queryString.stringify({
+				action: 'wpbr_get_reviews',
+				platform: platform,
+				reviewSourceId: reviewSourceId
+			})
+		)
+			.then( response => {
+				if ( response.data && 0 < response.data.length ) {
+					console.log( 'response received' );
+					console.log( response.data );
+
+					// Emit custom event that passes the request response.
+					this.emitter.emit( 'wpbrAfterGetReviews', 'response.data' );
+				} else {
+
+					// TODO: No reviews exist, so display message.
+				}
+			})
+			.catch( error => {
+
+				// TODO: Handle error of failed request.
+			});
 	}
 }
 
