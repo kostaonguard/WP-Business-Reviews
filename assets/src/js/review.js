d715216b7306b83ffb6913b69e897c8bc3d08bc3
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-17 04:37:34 -0500

    Add properties to Review JS class

--- assets/src/js/review.js
+++ assets/src/js/review.js
@@ -3,6 +3,18 @@ import truncate from 'lodash.truncate';
 class Review {
 	constructor( element ) {
 		this.root  = element;
+		this.reviewImage     = this.root.querySelector( '.js-wpbr-review-image' );
+		this.reviewRating    = this.root.querySelector( '.js-wpbr-review-rating' );
+		this.reviewTimestamp = this.root.querySelector( '.js-wpbr-review-timestamp' );
+		this.reviewContent   = this.root.querySelector( '.js-wpbr-review-content' );
+	}
+
+	hide( element ) {
+		element.classList.add( 'wpbr-u-hidden' );
+	}
+
+	show( element ) {
+		element.classList.remove( 'wpbr-u-hidden' );
 	}
 }
 
