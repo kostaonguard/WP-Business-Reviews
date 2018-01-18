d6171b5ccee8a74163efcda7df5cbe6bea061070
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-18 13:39:30 -0500

    Complete normalization of Yelp reviews

--- includes/request/class-yelp-request.php
+++ includes/request/class-yelp-request.php
@@ -73,11 +73,9 @@ class Yelp_Request extends Request {
 		$response = $this->get( $url, $args );
 
 		if ( isset( $response['businesses'] ) ) {
-			foreach( $response['businesses'] as $review_source ) {
-				$review_sources[] = $this->normalize_review_source( $review_source );
-			}
+			return $response['businesses'];
 		} else {
-			return new \WP_Error( 'invalid_response_structure', __( 'Response could not be normalized due to invalid response structure.', 'wp-business-reviews' ) );
+			return new \WP_Error( 'invalid_response_structure', __( 'Invalid response structure.', 'wp-business-reviews' ) );
 		}
 
 		return $review_sources;
@@ -107,95 +105,6 @@ class Yelp_Request extends Request {
 	}
 
 	/**
-	 * Normalizes and sanitize a raw review source from the platform API.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param array $raw_review_source Review source data from platform API.
-	 * @return array|WP_Error Standardized review source properties or WP_Error
-	 *                        if response structure is invalid.
-	 */
-	public function normalize_review_source( array $raw_review_source ) {
-		$r = $raw_review_source;
-		$r_clean = array();
-
-		// Set ID of the review source on the platform.
-		if ( isset( $r['id'] ) ) {
-			$r_clean['review_source_id'] =  $this->clean( $r['id'] );
-		}
-
-		// Set name.
-		if ( isset( $r['name'] ) ) {
-			$r_clean['name'] =  $this->clean( $r['name'] );
-		}
-
-		// Set page URL.
-		if ( isset( $r['url'] ) ) {
-			$r_clean['url'] = $this->clean( $r['url'] );
-		}
-
-		// Set rating.
-		if ( isset( $r['rating'] ) ) {
-			$r_clean['rating'] = $this->clean( $r['rating'] );
-		}
-
-		// Set image.
-		if ( isset( $r['image_url'] ) ) {
-			$r_clean['image'] = $this->modify_image_size( $this->clean( $r['image_url'] ) );
-		}
-
-		// Set phone.
-		if ( isset( $r['display_phone'] ) ) {
-			$r_clean['phone'] =  $this->clean( $r['display_phone'] );
-		}
-
-		// Set formatted address.
-		if ( isset( $r['location']['display_address'] ) ) {
-			$r_clean['formatted_address'] = $this->format_address( $this->clean( $r['location']['display_address'] ) );
-		}
-
-		// Set street address.
-		if ( isset( $r['location']['address1'] ) ) {
-			$r_clean['street_address'] = $this->clean( $r['location']['address1'] );
-		}
-
-		// Set city.
-		if ( isset( $r['location']['city'] ) ) {
-			$r_clean['city'] = $this->clean( $r['location']['city'] );
-		}
-
-		// Set state.
-		if ( isset( $r['location']['state'] ) ) {
-			$r_clean['state_province'] = $this->clean( $r['location']['state'] );
-		}
-
-		// Set postal code.
-		if ( isset( $r['location']['zip_code'] ) ) {
-			$r_clean['postal_code'] = $this->clean( $r['location']['zip_code'] );
-		}
-
-		// Set country.
-		if ( isset( $r['location']['country'] ) ) {
-			$r_clean['country'] = $this->clean( $r['location']['country'] );
-		}
-
-		// Set latitude.
-		if ( isset( $r['coordinates']['latitude']) ) {
-			$r_clean['latitude'] = $this->clean( $r['coordinates']['latitude'] );
-		}
-
-		// Set longitude.
-		if ( isset( $r['coordinates']['longitude']) ) {
-			$r_clean['longitude'] = $this->clean( $r['coordinates']['longitude'] );
-		}
-
-		// Merge clean response values with default values in case any values were not provided.
-		$review_source = $this->args = wp_parse_args( $r_clean, $this->get_review_source_defaults() );
-
-		return $review_source;
-	}
-
-	/**
 	 * Retrieves reviews based on Yelp business ID.
 	 *
 	 * @since 0.1.0
@@ -215,34 +124,12 @@ class Yelp_Request extends Request {
 
 		$response = $this->get( $url, $args );
 
+		if ( isset( $response['reviews'] ) ) {
+			return $response['reviews'];
+		} else {
+			return new \WP_Error( 'invalid_response_structure', __( 'Invalid response structure.', 'wp-business-reviews' ) );
+		}
+
 		return $response;
-	}
-
-	/**
-	 * Modify the image URL from API response.
-	 *
-	 * The image returned by the Yelp Fusion API is 1000px wide, which is
-	 * unnecessarily big for this plugin's purposes. Changing the suffix
-	 * results in a more appropriate size.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $image Image URL.
-	 * @return string Modified image URL.
-	 */
-	protected function modify_image_size( $image ) {
-		return str_replace( 'o.jpg', 'l.jpg', $image );
-	}
-
-	/**
-	 * Formats address from separate address components.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param array $address_components Associative array of address strings.
-	 * @return string Formatted address.
-	 */
-	protected function format_address( $address_components ) {
-		return trim( implode( $address_components, ', ' ) );
 	}
 }
--- includes/request/response-normalizer/class-yelp-response-normalizer.php
+++ includes/request/response-normalizer/class-yelp-response-normalizer.php
@@ -28,7 +28,75 @@ class Yelp_Response_Normalizer extends Response_Normalizer_Abstract {
 		$r          = $raw_review_source;
 		$normalized = array();
 
-		// Normalize...
+		// Set ID of the review source on the platform.
+		if ( isset( $r['id'] ) ) {
+			$normalized['review_source_id'] =  $this->clean( $r['id'] );
+		}
+
+		// Set name.
+		if ( isset( $r['name'] ) ) {
+			$normalized['name'] =  $this->clean( $r['name'] );
+		}
+
+		// Set page URL.
+		if ( isset( $r['url'] ) ) {
+			$normalized['url'] = $this->clean( $r['url'] );
+		}
+
+		// Set rating.
+		if ( isset( $r['rating'] ) ) {
+			$normalized['rating'] = $this->clean( $r['rating'] );
+		}
+
+		// Set image.
+		if ( isset( $r['image_url'] ) ) {
+			$normalized['image'] = $this->modify_image_size( $this->clean( $r['image_url'] ) );
+		}
+
+		// Set phone.
+		if ( isset( $r['display_phone'] ) ) {
+			$normalized['phone'] =  $this->clean( $r['display_phone'] );
+		}
+
+		// Set formatted address.
+		if ( isset( $r['location']['display_address'] ) ) {
+			$normalized['formatted_address'] = $this->format_address( $this->clean( $r['location']['display_address'] ) );
+		}
+
+		// Set street address.
+		if ( isset( $r['location']['address1'] ) ) {
+			$normalized['street_address'] = $this->clean( $r['location']['address1'] );
+		}
+
+		// Set city.
+		if ( isset( $r['location']['city'] ) ) {
+			$normalized['city'] = $this->clean( $r['location']['city'] );
+		}
+
+		// Set state.
+		if ( isset( $r['location']['state'] ) ) {
+			$normalized['state_province'] = $this->clean( $r['location']['state'] );
+		}
+
+		// Set postal code.
+		if ( isset( $r['location']['zip_code'] ) ) {
+			$normalized['postal_code'] = $this->clean( $r['location']['zip_code'] );
+		}
+
+		// Set country.
+		if ( isset( $r['location']['country'] ) ) {
+			$normalized['country'] = $this->clean( $r['location']['country'] );
+		}
+
+		// Set latitude.
+		if ( isset( $r['coordinates']['latitude']) ) {
+			$normalized['latitude'] = $this->clean( $r['coordinates']['latitude'] );
+		}
+
+		// Set longitude.
+		if ( isset( $r['coordinates']['longitude']) ) {
+			$normalized['longitude'] = $this->clean( $r['coordinates']['longitude'] );
+		}
 
 		// Merge normalized properties with default properites in case any are missing.
 		$normalized = wp_parse_args( $normalized, $this->get_review_source_defaults() );
@@ -43,11 +111,64 @@ class Yelp_Response_Normalizer extends Response_Normalizer_Abstract {
 		$r          = $raw_review;
 		$normalized = array();
 
-		// Normalize...
+		// Set reviewer.
+		if ( isset( $r['user']['name'] ) ) {
+			$normalized['reviewer'] = $this->clean( $r['user']['name'] );
+		}
+
+		// Set reviewer image.
+		if ( isset( $r['user']['image_url'] ) ) {
+			$normalized['reviewer_image'] = $this->modify_image_size(
+				$this->clean( $r['user']['image_url'] )
+			);
+		}
+
+		// Set rating.
+		if ( isset( $r['rating'] ) ) {
+			$normalized['rating'] = $this->clean( $r['rating'] );
+		}
+
+		// Set timestamp.
+		if ( isset( $r['time_created'] ) ) {
+			$normalized['timestamp'] = $this->clean( $r['time_created'] );
+		}
+
+		// Set content.
+		if ( isset( $r['text'] ) ) {
+			$normalized['content'] = $this->clean( $r['text'] );
+		}
 
 		// Merge normalized properties with default properites in case any are missing.
 		$normalized = wp_parse_args( $normalized, $this->get_review_defaults() );
 
 		return $normalized;
+	}
+
+	/**
+	 * Modify the image URL from API response.
+	 *
+	 * The image returned by the Yelp Fusion API is 1000px wide, which is
+	 * unnecessarily big for this plugin's purposes. Changing the suffix
+	 * results in a more appropriate size.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $image Image URL.
+	 * @return string Modified image URL.
+	 */
+	protected function modify_image_size( $image ) {
+		return str_replace( 'o.jpg', 'l.jpg', $image );
+	}
+
+	/**
+	 * Formats address from separate address components.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $address_components Associative array of address strings.
+	 * @return string Formatted address.
+	 */
+	protected function format_address( $address_components ) {
+		return trim( implode( $address_components, ', ' ) );
 	}
 }
