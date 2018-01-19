333da8217003b88fe982d9fba8eeda73ec5afca6
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-19 03:58:57 -0500

    Add methods to replace/add/clear reviews

--- assets/src/js/review-collection.js
+++ assets/src/js/review-collection.js
@@ -2,26 +2,43 @@ import Review from './review';
 
 class ReviewCollection {
 	constructor( element ) {
-		this.root    = element;
-		this.list    = this.root.querySelector( '.js-wpbr-list' );
-		this.items   = this.root.querySelectorAll( '.js-wpbr-item' );
-		this.reviews = new Set();
+		this.root = element;
 	}
 
 	init() {
-		const reviewEls = this.root.querySelectorAll( '.js-wpbr-review' );
+		this.list    = this.root.querySelector( '.js-wpbr-list' );
+		this.reviews = new Set();
+	}
 
-		for ( const reviewEl of reviewEls ) {
-			this.reviews.add( new Review( reviewEl ) );
+	replaceReviews( reviewsData ) {
+		this.clearReviews();
+		this.addReviews( reviewsData );
+		this.renderReviews();
+	}
+
+	addReviews( reviewsData ) {
+		for ( const reviewData of reviewsData ) {
+			const reviewObj = new Review( reviewData );
+			this.reviews.add( reviewObj );
 		}
 	}
 
-	updateReviews( reviewsData ) {
-		console.table( reviewsData );
-		for ( const review of reviewsData ) {
+	clearReviews() {
+		this.reviews.clear();
+		this.list.innerHTML = '';
+	}
 
-			// TODO: Update review content.
+	renderReviews() {
+		const fragment = document.createDocumentFragment();
+
+		for ( const review of this.reviews ) {
+			const li     = document.createElement( 'li' );
+			li.className = 'wpbr-review-gallery__item wpbr-review-gallery__item--2';
+			li.innerHTML = review.render();
+			fragment.appendChild( li );
 		}
+
+		this.list.appendChild( fragment );
 	}
 }
 
