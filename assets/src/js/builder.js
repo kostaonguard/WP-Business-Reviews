9a287f3600d2b26a3344bae4a46c21a8fe14a410
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-19 06:28:00 -0500

    Shift presentation logic to ReviewCollection

--- assets/src/js/builder.js
+++ assets/src/js/builder.js
@@ -26,13 +26,18 @@ class Builder {
 	}
 
 	initInspector() {
+		this.registerInspectorEventHandlers();
 		this.inspector = new Inspector ( document.getElementById( 'wpbr-builder-inspector' ) );
 		this.inspector.init();
-		this.registerInspectorEventHandlers();
 	}
 
 	initReviewCollection() {
-		this.reviewCollection = new ReviewCollection( document.querySelector( '.js-wpbr-wrap' ) );
+		this.reviewCollection = new ReviewCollection(
+			document.querySelector( '.js-wpbr-wrap' ),
+			this.inspector.fields.get( 'format' ).value,
+			this.inspector.fields.get( 'max_columns' ).value,
+			this.inspector.fields.get( 'theme' ).value,
+		);
 		this.reviewCollection.init();
 		this.registerReviewCollectionEventHandlers();
 	}
@@ -83,95 +88,62 @@ class Builder {
 
 	reflectControlChange( controlId, controlValue ) {
 		switch ( controlId ) {
+
 		case 'format' :
-			this.format( controlValue );
+			this.reviewCollection.format = controlValue;
+			this.reviewCollection.updatePresentation();
 
 			if ( 'review_gallery' === controlValue ) {
-				this.inspector.fields.get( 'max_columns' ).show();
+				// this.inspector.fields.get( 'max_columns' ).show();
 			} else {
-				this.inspector.fields.get( 'max_columns' ).hide();
+				// this.inspector.fields.get( 'max_columns' ).hide();
 			}
+
 			break;
+
 		case 'max_columns':
-			this.formatGallery( controlValue );
+			this.reviewCollection.maxColumns = controlValue;
+			this.reviewCollection.updatePresentation();
 			break;
+
 		case 'theme':
-			this.applyTheme( controlValue );
+			this.reviewCollection.theme = controlValue;
+			this.reviewCollection.updatePresentation();
+			this.updateTheme( controlValue );
 			break;
+
 		case 'review_image':
 			for ( const review of this.reviewCollection.reviews ) {
 				toggleVisibility( review.image );
 			}
 			break;
+
 		case 'review_rating':
 			for ( const review of this.reviewCollection.reviews ) {
 				toggleVisibility( review.rating );
 			}
 			break;
+
 		case 'review_timestamp':
 			for ( const review of this.reviewCollection.reviews ) {
 				toggleVisibility( review.timestamp );
 			}
 			break;
+
 		case 'review_content':
 			for ( const review of this.reviewCollection.reviews ) {
 				toggleVisibility( review.content );
 			}
 			break;
+
 		}
 	}
 
-	format( type ) {
-		switch ( type ) {
-
-		case 'review_gallery':
-			this.formatGallery( this.inspector.fields.get( 'max_columns' ).value );
-			break;
-
-		case 'review_list':
-			this.formatList();
-			break;
-
-		case 'review_carousel':
-			this.formatCarousel();
-			break;
-
-		case 'business_badge':
-			this.formatBadge();
-			break;
-		}
-	}
-
-	formatGallery( columns = 3 ) {
-		this.reviewCollection.list.classList = 'wpbr-review-gallery';
-
-		for ( const item of this.reviewCollection.items ) {
-			item.className = `wpbr-review-gallery__item wpbr-review-gallery__item--${columns} js-wpbr-item`;
-		}
-	}
-
-	formatList() {
-		this.reviewCollection.list.classList = 'wpbr-stacked-list';
-		this.reviewCollection.items.forEach( item => {
-			item.className = 'wpbr-stacked-list__item js-wpbr-item';
-		});
-	}
-
-	formatCarousel() {
-
-	}
-
-	formatBadge() {
-
-	}
-
-	applyTheme( theme = 'card' ) {
-		this.reviewCollection.root.className = `wpbr-wrap wpbr-theme--${theme} js-wpbr-wrap`;
-
+	updateTheme( theme ) {
 		if ( 'seamless-dark' === theme ) {
-			this.backgroundElement.classList.add( 'wpbr-theme--dark' );
+			this.background.classList.add( 'wpbr-theme--dark' );
 		} else {
-			this.backgroundElement.classList.remove( 'wpbr-theme--dark' );
+			this.background.classList.remove( 'wpbr-theme--dark' );
 		}
 	}
 }
