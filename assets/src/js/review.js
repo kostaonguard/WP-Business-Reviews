9e0392919945d171898e50c04e4e1e8ef5e6367b
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-19 17:34:45 -0500

    Reference class properties directly when rendering

--- assets/src/js/review.js
+++ assets/src/js/review.js
@@ -15,32 +15,32 @@ class Review {
 		return `
 			<div class="wpbr-review">
 				<div class="wpbr-review__header">
-					${this.image ? this.renderImage( this.image ) : ''}
+					${this.image ? this.renderImage() : ''}
 					<div class="wpbr-review__details">
 						<h3 class="wpbr-review__name js-wpbr-review-name">${this.reviewer}</h3>
 						${0 < this.rating ? stars.generateStars( this.rating, this.platform ) : ''}
-						${this.timestamp ? this.renderTimestamp( this.timestamp ) : ''}
+						${this.timestamp ? this.renderTimestamp() : ''}
 					</div>
 				</div>
-				${this.content ? this.renderContent( this.content ) : ''}
+				${this.content ? this.renderContent() : ''}
 			</div>
 		`;
 	}
 
-	renderImage( image ) {
-		if ( 'placeholder' === image ) {
+	renderImage() {
+		if ( 'placeholder' === this.image ) {
 			return '<div class="wpbr-review__image wpbr-review__image--placeholder"></div>';
 		} else {
-			return `<div class="wpbr-review__image"><img src="${image}"></div>`;
+			return `<div class="wpbr-review__image"><img src="${this.image}"></div>`;
 		}
 	}
 
-	renderTimestamp( timestamp ) {
-		return `<span class="wpbr-review__timestamp">${timestamp}</span>`;
+	renderTimestamp() {
+		return `<span class="wpbr-review__timestamp">${this.timestamp}</span>`;
 	}
 
-	renderContent( content ) {
-		const arrayOfStrings = content.split( '\n' );
+	renderContent() {
+		const arrayOfStrings = this.content.split( '\n' );
 		const paragraphs = `${arrayOfStrings.map( string => `<p>${string}</p>` ).join( '' )}`;
 
 		return `<div class="wpbr-review__content">${paragraphs}</div>`;
