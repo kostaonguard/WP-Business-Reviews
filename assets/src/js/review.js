6aef47a061bba2bf860bf29542eb5ae977b74649
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-22 14:40:39 -0500

    Document Review JS class

--- assets/src/js/review.js
+++ assets/src/js/review.js
@@ -1,7 +1,24 @@
+/**
+ * Defines the Review class.
+ *
+ * @link https://wpbusinessreviews.com
+ *
+ * @since 0.1.0
+ */
+
 import * as stars from './stars';
 import truncate from 'lodash.truncate';
 
+/** Class representing a review. */
 class Review {
+
+	/**
+	 * Instantiates a Review object.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param {array} data Review data in JSON format.
+	 */
 	constructor( data ) {
 		this.platform  = data.platform;
 		this.reviewer  = data.reviewer;
@@ -11,6 +28,13 @@ class Review {
 		this.content   = data.content;
 	}
 
+	/**
+	 * Renders the review.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @returns string Review markup.
+	 */
 	render() {
 		return `
 			<div class="wpbr-review">
@@ -27,6 +51,16 @@ class Review {
 		`;
 	}
 
+	/**
+	 * Renders the review image.
+	 *
+	 * If the value of the image property is `placeholder`, then a placeholder
+	 * div is rendered. Otherwise an image element is rendered.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @returns string Review image markup.
+	 */
 	renderImage() {
 		if ( 'placeholder' === this.image ) {
 			return '<div class="wpbr-review__image wpbr-review__image--placeholder"></div>';
@@ -35,10 +69,27 @@ class Review {
 		}
 	}
 
+	/**
+	 * Renders the review timestamp.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @returns string Review timestamp markup.
+	 */
 	renderTimestamp() {
 		return `<span class="wpbr-review__timestamp">${this.timestamp}</span>`;
 	}
 
+	/**
+	 * Renders the review content.
+	 *
+	 * If the content string contains lines breaks, the string will be parsed
+	 * and rendered as multiple paragraphs.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @returns string Review content markup.
+	 */
 	renderContent() {
 		const arrayOfStrings = this.content.split( '\n' );
 		const paragraphs = `${arrayOfStrings.map( string => `<p>${string}</p>` ).join( '' )}`;
