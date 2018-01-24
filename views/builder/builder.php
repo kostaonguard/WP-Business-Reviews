1f0cb753dba850f8b27da111f7edc3bf9a1d9516
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-23 23:45:17 -0500

    Wrap builder in form so data can be saved

--- views/builder/builder.php
+++ views/builder/builder.php
@@ -1,21 +1,23 @@
 <div id="wpbr-builder" class="wpbr-builder js-wpbr-builder">
-	<div class="wpbr-builder__toolbar">
-		<div id="titlediv" class="wpbr-builder__title ">
-			<div id="titlewrap">
-				<input type="text" name="post_title" size="30" value="" id="title" spellcheck="true" autocomplete="off" placeholder="Add title">
+	<form method="post" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
+		<div class="wpbr-builder__toolbar">
+			<div id="titlediv" class="wpbr-builder__title ">
+				<div id="titlewrap">
+					<input type="text" name="post_title" size="30" value="" id="title" spellcheck="true" autocomplete="off" placeholder="Add title">
+				</div>
+			</div>
+			<div class="wpbr-builder__tools">
+				<ul class="wpbr-inline-list">
+					<li class="wpbr-inline-list__item"><button id="wpbr-control-inspector" class="button"><?php esc_html_e( 'Settings', 'wp-business-reviews' ); ?></button></li>
+					<li class="wpbr-inline-list__item"><button id="wpbr-control-save" class="button button-primary"><?php esc_html_e( 'Save Review Set', 'wp-business-reviews' ); ?></button></li>
+				</ul>
 			</div>
 		</div>
-		<div class="wpbr-builder__tools">
-			<ul class="wpbr-inline-list">
-				<li class="wpbr-inline-list__item"><button id="wpbr-control-inspector" class="button"><?php esc_html_e( 'Settings', 'wp-business-reviews' ); ?></button></li>
-				<li class="wpbr-inline-list__item"><button id="wpbr-control-save" class="button button-primary"><?php esc_html_e( 'Save Review Set', 'wp-business-reviews' ); ?></button></li>
-			</ul>
+		<div class="wpbr-builder__workspace">
+			<?php
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/builder/builder-preview.php' );
+			$this->render_partial( WPBR_PLUGIN_DIR . 'views/builder/builder-inspector.php' );
+			?>
 		</div>
-	</div>
-	<div class="wpbr-builder__workspace">
-		<?php
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/builder/builder-preview.php' );
-		$this->render_partial( WPBR_PLUGIN_DIR . 'views/builder/builder-inspector.php' );
-		?>
-	</div>
+	</form>
 </div>
