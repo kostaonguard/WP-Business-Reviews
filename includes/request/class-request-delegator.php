13c77f439b5ebe7a61662660aadfe4d4f92316f9
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-01-26 00:22:10 -0500

    Refactor Request_Delegator to better handle Ajax
    Previously the Request and Response_Normalizer objects were being
    created within each method, which could unnecessarily result in multiple
    instances of those objects.
    
    Now an init() method instantiates one Request and one Request_Normalizer
    which can be used throughout the class methods. In addition, Ajax
    methods have been refactored to call the init() method prior to making a
    remote request.

--- includes/request/class-request-delegator.php
+++ includes/request/class-request-delegator.php
@@ -24,24 +24,58 @@ class Request_Delegator {
 	 *
 	 * @since 0.1.0
 	 *
-	 * @var string $request_factory
+	 * @var Request_Factory $request_factory
 	 */
 	private $request_factory;
+
+	/**
+	 * Factory that creates response normalizers.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var Response_Normalizer_Factory $normalizer_factory
+	 */
+	private $normalizer_factory;
+
+	/**
+	 * Request that retrieves data from remote API.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var Request $request
+	 */
+	private $request;
+
+	/**
+	 * Response normalizer that sanitizes and normalizes raw API responses.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var Response_Normalizer_Abstract $normalizer
+	 */
+	private $normalizer;
 
 	/**
 	 * Instantiates the Request_Delegator object.
 	 *
 	 * @since 0.1.0
 	 *
-	 * @param Request_Factory             $request_factory             Request factory.
-	 * @param Response_Normalizer_Factory $response_normalizer_factory Response normalizer factory.
+	 * @param Request_Factory             $request_factory    Request factory.
+	 * @param Response_Normalizer_Factory $normalizer_factory Normalizer factory.
 	 */
 	public function __construct(
 		Request_Factory $request_factory,
-		Response_Normalizer_Factory $response_normalizer_factory
+		Response_Normalizer_Factory $normalizer_factory
 	) {
-		$this->request_factory             = $request_factory;
-		$this->response_normalizer_factory = $response_normalizer_factory;
+		$this->request_factory    = $request_factory;
+		$this->normalizer_factory = $normalizer_factory;
+		$this->request            = null;
+		$this->normalizer         = null;
+	}
+
+	public function init( $platform ) {
+		$this->request    = $this->request_factory->create( $platform );
+		$this->normalizer = $this->normalizer_factory->create( $platform );
 	}
 
 	/**
@@ -51,7 +85,73 @@ class Request_Delegator {
 	 */
 	public function register() {
 		add_action( 'wp_ajax_wpbr_search_review_source', array( $this, 'ajax_search_review_source' ) );
-		add_action( 'wp_ajax_wpbr_get_reviews', array( $this, 'ajax_get_reviews' ) );
+		add_action( 'wp_ajax_wpbr_get_source_and_reviews', array( $this, 'ajax_get_source_and_reviews' ) );
+	}
+
+	/**
+	 * Searches a remote reviews platform based on platform and search query.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $terms    The search terms.
+	 * @param string $location The search location.
+	 * @return array Normalized, sanitized response body.
+	 */
+	public function search_review_source( $terms, $location ) {
+		$raw_response = $this->request->search_review_source( $terms, $location );
+
+		if ( is_wp_error( $raw_response ) ) {
+			return $raw_response->get_error_message();
+		}
+
+		$normalized_response = $this->normalizer->normalize_review_sources(
+			$raw_response
+		);
+
+		return $normalized_response;
+	}
+
+	/**
+	 * Requests review source based on the provided platform and review source ID.
+	 *
+	 * A review source is the business or entity with which individual reviews
+	 * are associated.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $review_source_id The review source ID.
+	 * @return array Normalized, sanitized response body.
+	 */
+	public function get_review_source( $review_source_id ) {
+		$raw_response = $this->request->get_review_source( $review_source_id );
+
+		if ( is_wp_error( $raw_response ) ) {
+			return $raw_response->get_error_message();
+		}
+
+		$normalized_response = $this->normalizer->normalize_review_source( $raw_response );
+
+		return $normalized_response;
+	}
+
+	/**
+	 * Requests reviews based on the provided platform and review source ID.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $review_source_id The review source ID.
+	 * @return array Normalized, sanitized response body.
+	 */
+	public function get_reviews( $review_source_id ) {
+		$raw_response = $this->request->get_reviews( $review_source_id );
+
+		if ( is_wp_error( $raw_response ) ) {
+			return $raw_response->get_error_message();
+		}
+
+		$normalized_response = $this->normalizer->normalize_reviews( $raw_response );
+
+		return $normalized_response;
 	}
 
 	/**
@@ -60,81 +160,60 @@ class Request_Delegator {
 	 * @since 0.1.0
 	 */
 	public function ajax_search_review_source() {
-		// TODO: Verify nonce and permission.
-
 		if ( ! isset( $_POST['platform'], $_POST['terms'], $_POST['location'] ) ) {
 			wp_die();
 		}
 
+		// TODO: Verify nonce and permission.
+
+		// Set request parameters from posted Ajax request.
 		$platform = sanitize_text_field( $_POST['platform'] );
 		$terms    = sanitize_text_field( $_POST['terms'] );
 		$location = sanitize_text_field( $_POST['location'] );
-		$response = $this->search_review_source( $platform, $terms, $location );
-		wp_send_json( $response );
+
+		// Initialize the request and normalizer based on the platform.
+		$this->init( $platform );
+
+		// Get review source data from remote API.
+		$review_source_array = $this->search_review_source( $terms, $location );
+
+		// Send it back as JSON.
+		wp_send_json( $review_source_array );
 	}
 
 	/**
-	 * Searches a remote reviews platform based on platform and search query.
+	 * Requests review source and reviews via Ajax post request.
 	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $platform The review platform ID.
-	 * @param string $terms    The search terms.
-	 * @param string $location The search location.
-	 * @return array Normalized, sanitized response body.
-	 */
-	public function search_review_source( $platform, $terms, $location ) {
-		$request             = $this->request_factory->create( $platform );
-		$normalizer          = $this->response_normalizer_factory->create( $platform );
-		$raw_response        = $request->search_review_source( $terms, $location );
-
-		if ( is_wp_error( $raw_response ) ) {
-			return $raw_response->get_error_message();
-		}
-
-		$normalized_response = $normalizer->normalize_review_sources( $raw_response );
-
-		return $normalized_response;
-	}
-
-	/**
-	 * Requests reviews based on the provided platform and review source ID.
+	 * Review source and its reviews are frequently requested together. This
+	 * function sends data for both in a single response.
 	 *
 	 * @since 0.1.0
 	 */
-	public function ajax_get_reviews() {
-		// TODO: Verify nonce and permission.
-
+	public function ajax_get_source_and_reviews() {
+		// Check for platform and review source ID in $_POST.
 		if ( ! isset( $_POST['platform'], $_POST['reviewSourceId'] ) ) {
 			wp_die();
 		}
 
+		// TODO: Verify nonce and permission.
+
+		// Set request parameters from posted Ajax request.
 		$platform         = sanitize_text_field( $_POST['platform'] );
 		$review_source_id = sanitize_text_field( $_POST['reviewSourceId'] );
-		$response = $this->get_reviews( $platform, $review_source_id );
-		wp_send_json( $response );
-	}
 
-	/**
-	 * Requests reviews based on the provided platform and review source ID.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $platform         The review platform ID.
-	 * @param string $review_source_id The review source ID.
-	 * @return array Normalized, sanitized response body.
-	 */
-	public function get_reviews( $platform, $review_source_id ) {
-		$request             = $this->request_factory->create( $platform );
-		$normalizer          = $this->response_normalizer_factory->create( $platform );
-		$raw_response        = $request->get_reviews( $review_source_id );
+		// Initialize the request and normalizer based on the platform.
+		$this->init( $platform );
 
-		if ( is_wp_error( $raw_response ) ) {
-			return $raw_response->get_error_message();
-		}
+		// Get review source and reviews data from remote API.
+		$review_source_array = $this->get_review_source( $review_source_id );
+		$reviews_array       = $this->get_reviews( $review_source_id );
 
-		$normalized_response = $normalizer->normalize_reviews( $raw_response );
-
-		return $normalized_response;
+		// Send it all back together as JSON.
+		wp_send_json(
+			array(
+				'review_source' => $review_source_array,
+				'reviews'       => $reviews_array,
+			)
+		);
 	}
 }
