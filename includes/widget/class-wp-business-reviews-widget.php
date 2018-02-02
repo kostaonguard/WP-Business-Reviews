a3631cf32699d594175b4808f2abb5803659aa32
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-02-02 14:47:54 -0500

    Document WP_Business_Reviews_Widget

--- includes/widget/class-wp-business-reviews-widget.php
+++ includes/widget/class-wp-business-reviews-widget.php
@@ -19,6 +19,8 @@ namespace WP_Business_Reviews\Includes\Widget;
  */
 class WP_Business_Reviews_Widget extends \WP_Widget {
 	/**
+	 * Instantiates the WP_Business_Reviews_Widget object.
+	 *
 	 * @since 0.1.0
 	 */
 	public function __construct() {
@@ -36,6 +38,8 @@ class WP_Business_Reviews_Widget extends \WP_Widget {
 	}
 
 	/**
+	 * Registers functionality with WordPress hooks.
+	 *
 	 * @since 0.1.0
 	 */
 	public function register() {
@@ -45,19 +49,41 @@ class WP_Business_Reviews_Widget extends \WP_Widget {
 	}
 
 	/**
+	 * Echoes the widget content.
+	 *
 	 * @since 0.1.0
+	 *
+	 * @param array $args     Display arguments including 'before_title', 'after_title',
+	 *                        'before_widget', and 'after_widget'.
+	 * @param array $instance The settings for the particular instance of the widget.
 	 */
 	public function widget( $args, $instance ) {
 	}
 
 	/**
+	 * Outputs the settings update form.
+	 *
 	 * @since 0.1.0
+	 *
+	 * @param array $instance Current settings.
+	 * @return string Default return is 'noform'.
 	 */
 	public function form( $instance ) {
 	}
 
 	/**
-	 * @since 0.1.0
+	 * Updates a particular instance of a widget.
+	 *
+	 * This function should check that `$new_instance` is set correctly. The newly-calculated
+	 * value of `$instance` should be returned. If false is returned, the instance won't be
+	 * saved/updated.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via
+	 *                            WP_Widget::form().
+	 * @param array $old_instance Old settings for this instance.
+	 * @return array|bool Settings to save or bool false to cancel saving.
 	 */
 	public function update( $new_instance, $old_instance ) {
 	}
