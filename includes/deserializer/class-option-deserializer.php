a3dd2e3c098a61a4c1846f2c2852393789aa57df
Kevin Hoffman <kevin@kevinwhoffman.com>
2018-02-05 18:11:38 -0500

    Remove the need for abstract deserializer class

--- includes/deserializer/class-option-deserializer.php
+++ includes/deserializer/class-option-deserializer.php
@@ -15,9 +15,24 @@ namespace WP_Business_Reviews\Includes\Deserializer;
  *
  * @since 0.1.0
  */
-class Option_Deserializer extends Deserializer_Abstract {
+class Option_Deserializer {
 	/**
+	 * The prefix prepended to the retrieved key.
+	 *
 	 * @since 0.1.0
+	 * @var string $prefix
+	 */
+	protected $prefix = 'wp_business_reviews_';
+
+	/**
+	 * Retrieves an option from the database.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $key  Name of option to retrieve.
+	 * @param mixed  $default Optional. Default value to return if the option does
+	 *                        not exist.
+	 * @return mixed Value associated with the key.
 	 */
 	public function get( $key, $default = false ) {
 		return get_option( $this->prefix . $key, $default );
